43ee0e36a49b6834efea6f743a9d5d59291724c2
rhealbarnett <rhealbarnett@gmail.com>
2017-07-28 10:55:33 +1000

    added varying n, B0 -- not working

--- disp_rels.py
+++ disp_rels.py
@@ -42,12 +42,28 @@ import matplotlib.pyplot as plt
 # k _|_ B0 : om^2/k^2 = c^2*((v_s^2 + va^2)/(c^2 + va^2))
 # --------------------------------------------------------- #
 
+import pdb
+
+r_max = 1.2
+r_min = 0.
+npts = 121
+r = np.linspace(r_min,r_max,npts+1)
+
 #-- magnetic field of 1T
-B0 = 1.
+B0_max = 3.4
+B0 = np.zeros(npts)
+for ii in range(0,npts):
+	B0[ii] = 1.0/(r_max - r[ii])
+	print ii, r[ii]
+#B0[npts-1] = max(B0)
+B0 = (B0/max(B0))*B0_max
+#pdb.set_trace()
+
+r = r[0:npts]
 
 #-- assume mi/me = 5
 me = 9.11e-31
-mi = 5.*me
+mi = 2.*1.67e-27
 #freq = 51.0e6
 #om = 2.*np.pi*freq
 
@@ -59,8 +75,18 @@ sign_e = q_e / abs(q_e)
 sign_i = q_i / abs(q_i)
 
 #-- assume Ni=Ne=N
-N = 1.0e18
-alpha = 3.
+N_max = 1.0e18
+N_min = 1.0e16
+#gradient = (N_min - N_max)/(r[0] - r[npts-1])
+#N = gradient*(r - r[npts-1]) + N_max
+#sig_sq = 0.2
+#c = np.sqrt(sig_sq)
+#b = 0.
+#a = N_max/(c*np.sqrt(2.*np.pi))
+#N = a*np.exp(((r - b)**2.)/(2.*c**2.))
+alpha = 35.
+N = np.exp(alpha*r) + N_min
+#alpha = 3.
 #N = alpha*eps0*(B0**2.)/(me)
 
 #-- plasma frequencies (assume ion has charge +e)
@@ -72,32 +98,42 @@ om_ce = abs(q_e)*B0/me
 om_ci = abs(q_i)*B0/mi
 
 #-- frequency values
-fact = np.logspace(-2,1, 100)
+#fact = np.logspace(-2,1, 100)
 #fact += 0.1*np.min(fact)
-om = fact*om_ce
+#om = fact*om_ce
+freq = 51.0e6
+om = 2.*np.pi*freq
 
 #-- solve dispersion relation n^2
 ns_R = 1.0 - ((om_pi**2)/(om*(om + om_ci))) - ((om_pe**2)/(om*(om - om_ce)))
 
-S = 1.0 - ((om_pe**2.)/(om**2. - om_ce**2.)) - ((om_pi**2.)/(om**2. - om_ci**2.))
-D = ((sign_e*om_ce*(om_pe**2.))/(om*(om**2. - om_ce**2.))) + ((sign_i*om_ci*(om_ce**2))/(om*(om**2. - om_ci**2.)))
+S = 1.0 - ((om_pe**2.)/(om**2. - om_ce**2.) + (om_pi**2.)/(om**2. - om_ci**2.))
+D = (sign_e*om_ce*om_pe**2.)/(om*(om**2. - om_ce**2.)) + (sign_i*om_ci*om_pi**2)/(om*(om**2. - om_ci**2.))
 P = 1.0 - ((om_pe**2. / om**2.) + (om_pi**2. / om**2.))
-R = 1.0 - (((om_pe**2.)/(om*(om + sign_e*om_ce))) + ((om_pi**2.)/(om*(om + sign_i*om_ci))))
-L = 1.0 - (((om_pe**2.)/(om*(om - sign_e*om_ce))) + ((om_pi**2.)/(om*(om - sign_i*om_ci))))
+#R = 1.0 - (((om_pe**2.)/(om*(om + sign_e*om_ce))) + ((om_pi**2.)/(om*(om + sign_i*om_ci))))
+#L = 1.0 - (((om_pe**2.)/(om*(om - sign_e*om_ce))) + ((om_pi**2.)/(om*(om - sign_i*om_ci))))
+R = S + D
+L = S - D
 
-theta = 0.
+theta = np.pi/2.
 
 A = S*np.sin(theta)**2. + P*np.cos(theta)**2.
 B = R*L*np.sin(theta)**2. + P*S*(1.0 + np.cos(theta)**2.)
 C = P*R*L
-#F = np.sqrt(((R*L - P*S)**2.)*np.sin(theta)**4. + 4.*P**2.*D**2.*np.cos(theta)**2.)
-F = np.sqrt(B**2. - 4.*A*C)
+F = np.sqrt(((R*L - P*S)**2.)*np.sin(theta)**4. + 4.*P**2.*D**2.*np.cos(theta)**2.)
+#F = np.sqrt(B**2. - 4.*A*C)
 
-ns = (B + F) / (2.*A)
+ns_pos = (B + F) / (2.*A)
+ns_neg = (B - F) / (2.*A)
+om_hybe_sq = (om_pe**2. + om_ce**2.)
+om_hybi_sq = (om_pi**2. + om_ci**2.)
+X_mode = ((om_pe**2)/(om**2.))*((om**2. - om_pe**2.)/(om**2. - om_hybe_sq))
 
 plt.figure(1)
-plt.plot(om/om_ce, ns_R)
-plt.xlabel('${\omega}/{\omega_{ce}}$', size=16)
-plt.ylabel('$n_R^2$', size=16)
-plt.xscale('log')
+#plt.plot(om/om_ce, ns_R)
+plt.plot(r, ns_pos, label='pos')
+plt.plot(r, ns_neg, label='neg')
+plt.xlabel('r', size=16)
+plt.ylabel('$n^2$', size=16)
+#plt.xscale('log')
 plt.show(1)
