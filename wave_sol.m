eb1a0a9c67e5b3b8cdda44f52c356b174961ee45
rhealbarnett <rhealbarnett@gmail.com>
2017-10-12 11:18:16 -0400

    time independent wave solver -- wavelength changes with spatial domain??

--- wave_sol.m
+++ wave_sol.m
@@ -23,16 +23,19 @@ k0 = om/c0;
 %--
 % define wavenumbers ky and kz (/m); use values given in van eester section IV?? No
 % others mentioned
-ky = 5.0;
+ky = 0.0;
 kz = 6.0;
 
 %--
-% density N0 -- set to zero for vacuum case
-npts = 10;
+% spatial domain
+npts = 1000;
 xmin = 0.0;
-xmax = 2.5;
+xmax = 10.0;
 xax = linspace(xmin,xmax,npts);
-dx = (xmax - xmin)/(npts-1);
+dx = (xmax - xmin)/(npts - 1);
+
+%--
+% density -- set to zero for vacuum case
 N0 = 0.0;
 
 %--
@@ -101,29 +104,124 @@ cpdt = [[s, -1i*d, 0.0]
         [1i*d, s, 0.0]
         [0.0, 0.0, p]];
     
+% cpdt_rot = r'*cpdt*r;
+    
 %--
 % set up rhs matrix (multiples E field)
 
-waveeq_mat = zeros(3.0*npts, 9.0*npts);
+% syms ky kz k0 dx
+% cpdt = sym('R%d%d',[3,3]);
 
-for ii=2,3.0*npts:3;
-    eq1_row = 3.0*ii - 2.0;
-    eq2_row = 3.0*ii - 1.0;
-    eq3_row = 3.0*ii;
-    for kk=1,npts;
-        
+waveeq_mat = zeros(3*npts, 3*npts);
+% waveeq_mat = sym('O%d%d', [3*npts,3*npts]);
+ii = 1;
+
+for eq1=1:3:3*npts
     
-    waveeq_mat(eq1_row,ii) = -1i*ky;
-    waveeq_mat(eq1_row,ii-1) = 0.0;
-    waveeq_mat(eq1_row,ii+1) = -1i*kz;
-    waveeq_mat(eq2_row,ii) = -1.0;
-    waveeq_mat(eq2_row,ii-1) = -1i*ky*(dx/2.0);
-    waveeq_mat(eq2_row,ii+1) = 0.0;
-    waveeq_mat(eq3_row,ii) = 0.0;
-    waveeq_mat(eq3_row,ii-1) = -1i*kz*(dx/2.0);
-    waveeq_mat(eq3_row,ii+1) = -1.0;
+    eq2 = eq1+1;
+    eq3 = eq2+1;
     
+    iiex = ii;
+    iiey = ii+1;
+    iiez = ii+2;
+    iiexm = iiex - 3;
+    iieym = iiey - 3;
+    iiezm = iiez - 3;
+    iiexp = iiex + 3;
+    iieyp = iiey + 3;
+    iiezp = iiez + 3;
+
+    %--
+    % set up periodic boundary conditions
+    if ((iiexm) & (iieym) & (iiezm)) <= 0
+        iiexm = 3*npts + iiexm;
+        iieym = 3*npts + iieym;
+        iiezm = 3*npts + iiezm;
+    end
     
+    %--
+    % this loop doesn't work if it's set up like the previous one???
+    if ((iiexp)) >= (3*npts)
+        iiexp = iiexp - 3*npts;
+        iieyp = iieyp - 3*npts;
+        iiezp = iiezp - 3*npts;
+    end          
+    
+    %--
+    % fill matrix
+    waveeq_mat(eq1,iiexm) = 0.0;
+    waveeq_mat(eq1,iieym) = -1i*ky;
+    waveeq_mat(eq1,iiezm) = -1i*kz;
+    waveeq_mat(eq1,iiex) = 2.0*dx*(ky^2 + kz^2 - k0^2*cpdt(1,1));
+    waveeq_mat(eq1,iiey) = -2.0*dx*k0^2*cpdt(1,2);
+    waveeq_mat(eq1,iiez) = -2.0*dx*k0^2*cpdt(1,3);
+    waveeq_mat(eq1,iiexp) = 0.0;
+    waveeq_mat(eq1,iieyp) = 1i*ky;
+    waveeq_mat(eq1,iiezp) = 1i*kz;
+    
+    waveeq_mat(eq2,iiexm) = -1i*ky*(dx/2.0);
+    waveeq_mat(eq2,iieym) = -1.0;
+    waveeq_mat(eq2,iiezm) = 0.0;
+    waveeq_mat(eq2,iiex) = -dx^2*k0^2*cpdt(2,1);
+    waveeq_mat(eq2,iiey) = dx^2*(kz^2 - k0^2*cpdt(2,2)) + 2.0;
+    waveeq_mat(eq2,iiez) = -dx^2*(ky*kz + k0^2*cpdt(2,3));
+    waveeq_mat(eq2,iiexp) = 1i*ky*(dx/2.0);
+    waveeq_mat(eq2,iieyp) = -1.0;
+    waveeq_mat(eq2,iiezp) = 0.0;
+    
+    waveeq_mat(eq3,iiexm) = -1i*kz*(dx/2.0);
+    waveeq_mat(eq3,iieym) = 0.0;
+    waveeq_mat(eq3,iiezm) = -1.0;
+    waveeq_mat(eq3,iiex) = -dx^2*k0^2*cpdt(3,1);
+    waveeq_mat(eq3,iiey) = -dx^2*(ky*kz + k0^2*cpdt(3,2));
+    waveeq_mat(eq3,iiez) = dx^2*(ky^2 - k0^2*cpdt(3,3)) + 2.0;
+    waveeq_mat(eq3,iiexp) = 1i*kz*(dx/2.0);
+    waveeq_mat(eq3,iieyp) = 0.0;
+    waveeq_mat(eq3,iiezp) = -1.0;
+    
+    ii = ii + 3;
+
+end
+
+%--
+% set up rhs vector
+rhs = zeros(3*npts,1);
+rhs(1501) = 0.0;
+rhs(1502) = 1.0;
+rhs(1503) = 1.0;
+
+%--
+% calculation solution as waveeq_mat^-1*rhs
+%-- COMMENT OUT IF DOING SYMBOLIC MATRIX --%
+sol = (waveeq_mat)\rhs;
+
+%----------------------plots-----------------------%
+figure(1)
+plot(xax, real(sol(1:3:3*npts)))
+
+hold on
+
+plot(xax, real(sol(2:3:3*npts)))
+plot(xax, real(sol(3:3:3*npts)))
+xlabel('Position')
+ylabel('Amplitude (?)')
+legend('Re[Ex] (?)', 'Re[Ey] (?)', 'Re[Ez] (?)')
+
+hold off
+
+figure(2)
+plot(xax, imag(sol(1:3:3*npts)))
+
+hold on
+
+plot(xax, imag(sol(2:3:3*npts)))
+plot(xax, imag(sol(3:3:3*npts)))
+xlabel('Position')
+ylabel('Amplitude (?)')
+legend('Im[Ex] (?)', 'Im[Ey] (?)', 'Im[Ez] (?)')
+
+hold off
+
 
 
 
