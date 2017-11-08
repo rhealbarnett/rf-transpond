79a2e172cfbd30cfd6cc43f27ac278c7c9c6399c
rhealbarnett <rhealbarnett@gmail.com>
2017-11-08 13:07:22 -0500

    added phiy and phiz calculation

--- poisson_sol.m
+++ poisson_sol.m
@@ -4,31 +4,52 @@
 % rlbarnett c3149416 191017                %
 %------------------------------------------%
 
-% function static_pot = poisson_sol(Ne, Nh, Nd, lamby, lambz, e, eps0, dx, npts);
+%function static_pot = poisson_sol(Ne, Nh, Nd, lamby, lambz, e, eps0, dx, npts)
 
-coeff_mat = zeros(npts,npts);
+coeff_mat = zeros(npts,3.0*npts);
 rhs = zeros(npts,1);
 
-mult = dx^2*(lamby^2 + lambz^2) - 2.0;
 rho = (dx^2*e/eps0)*(Ne - (Nh + Nd));
+
+kk = 1;
 
 for ii=1:npts
     
-    iip = ii + 1;
-    iim = ii - 1;
+    iix = kk;
+    iiy = kk + 1;
+    iiz = kk + 2;
+    iixm = iix - 3;
+    iiym = iiy - 3;
+    iizm = iiz - 3;
+    iixp = iix + 3;
+    iiyp = iiy + 3;
+    iizp = iiz + 3;
     
-    if iip > npts
-        iip = iip - npts;
+    
+    if ((iixp)) > 3.0*npts
+        iixp = iixp - 3.0*npts;
+        iiyp = iiyp - 3.0*npts;
+        iizp = iizp - 3.0*npts;
     end
-    if iim <= 0
-        iim = npts + iim;
+    if ((iixm) & (iiym) & (iizm)) <= 0
+        iixm = 3.0*npts + iixm;
+        iiym = 3.0*npts + iiym;
+        iizm = 3.0*npts + iizm;
     end
     
-    coeff_mat(ii,ii) = mult;
-    coeff_mat(ii,iim) = 1.0;
-    coeff_mat(ii,iip) = 1.0;
+    coeff_mat(ii,iixm) = 1.0;
+    coeff_mat(ii,iiym) = 0.0;
+    coeff_mat(ii,iizm) = 0.0;
+    coeff_mat(ii,iix) = -2.0;
+    coeff_mat(ii,iiy) = dx^2*lamby^2;
+    coeff_mat(ii,iiz) = dx^2*lambz^2;
+    coeff_mat(ii,iixp) = 1.0;
+    coeff_mat(ii,iiyp) = 0.0;
+    coeff_mat(ii,iizp) = 0.0;
     
     rhs(ii) = rho;
+    
+    kk = kk + 3;
     
 end
 
@@ -36,7 +57,7 @@ coeff_mat = sparse(coeff_mat);
 
 static_pot = coeff_mat\rhs;
 
-% end
+%end
     
     
 
