ae122cb7840aaf45a041c0adae0b399974686f02
rhealbarnett <rhealbarnett@gmail.com>
2017-11-13 17:28:34 -0500

    calculate ponderomotive acceleration for 1D dve loop

---
+++ a_pond.m
@@ -0,0 +1,6 @@
+%------------------------------------------%
+% calculate ponderomotive acceleration     %
+% expression from DVE 2015                 %
+% rlbarnett c3149416 131117                %
+%------------------------------------------%
+
