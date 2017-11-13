6e5e199d1b47eaeac0cea166dd18178479ce7bcf
rhealbarnett <rhealbarnett@gmail.com>
2017-11-13 11:08:16 -0500

    calculation for static electric field components

--- static_e.m
+++ static_e.m
@@ -4,3 +4,14 @@
 % rlbarnett c3149416 071117                %
 %------------------------------------------%
 
+static_ey = -lamby*static_poty;
+static_ez = -lambz*static_potz;
+
+static_ex = zeros(npts,1);
+
+for ii=2,(npts-1);
+    static_ex(ii) = (static_potx(ii-1) - static_potx(ii+1))/(2.0*dx);
+end
+
+static_ex(1) = (-3.0*static_potx(1) + 4.0*static_potx(2) - static_pot(3))/(2.0*dx);
+static_ex(npts) = (3.0*static_potx(npts) - 4.0*static_potx(npts-1) + static_potx(npts-2))/(2.0*dx);
