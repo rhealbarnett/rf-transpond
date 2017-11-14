06410c30faa4f93f2370107fc17c53c6370e9caf
rhealbarnett <rhealbarnett@gmail.com>
2017-11-14 11:14:40 -0500

    apond calculation doesn't have three components.. check vector ops

--- a_pond.m
+++ a_pond.m
@@ -4,3 +4,43 @@
 % rlbarnett c3149416 131117                %
 %------------------------------------------%
 
+rf_E = zeros(npts,3);
+rf_E(:,1) = rf_ex;
+rf_E(:,2) = rf_ey;
+rf_E(:,3) = rf_ez;
+
+om_ce_vec = e*Bvec / me;
+om_ch_vec = qh*Bvec / mh;
+om_cd_vec = qd*Bvec / md;
+
+ee_vec = e*rf_E / me;
+eh_vec = qh*rf_E / mh;
+ed_vec = qd*rf_E / md;
+
+ee_mag = sqrt(ee_vec(:,1).*conj(ee_vec(:,1)) + ee_vec(:,2).*conj(ee_vec(:,2)) + ee_vec(:,3).*conj(ee_vec(:,3)));
+eh_mag = sqrt(eh_vec(:,1).*conj(eh_vec(:,1)) + eh_vec(:,2).*conj(eh_vec(:,2)) + eh_vec(:,3).*conj(eh_vec(:,3)));
+ed_mag = sqrt(ed_vec(:,1).*conj(ed_vec(:,1)) + ed_vec(:,2).*conj(ed_vec(:,2)) + ed_vec(:,3).*conj(ed_vec(:,3)));
+
+% om_ce_mag = sqrt(om_ce_vec(:,1).*conj(om_ce_vec(:,1)) + om_ce_vec(:,2).*conj(om_ce_vec(:,2)) + om_ce_vec(:,3).*conj(om_ce_vec(:,3)));
+% om_ch_mag = sqrt(om_ch_vec(:,1).*conj(om_ch_vec(:,1)) + om_ch_vec(:,2).*conj(om_ch_vec(:,2)) + om_ch_vec(:,3).*conj(om_ch_vec(:,3)));
+% om_cd_mag = sqrt(om_cd_vec(:,1).*conj(om_cd_vec(:,1)) + om_cd_vec(:,2).*conj(om_cd_vec(:,2)) + om_cd_vec(:,3).*conj(om_cd_vec(:,3)));
+
+producte = zeros(npts,1);
+producth = zeros(npts,1);
+productd = zeros(npts,1);
+
+for ii=1:npts
+    
+    producte(ii) = dot(ee_vec(ii,:),om_ce_vec);
+    producth(ii) = dot(eh_vec(ii,:),om_ch_vec);
+    productd(ii) = dot(ed_vec(ii,:),om_cd_vec);
+    
+end
+
+a_ponde = (1.0 / 4.0)*(1.0 / (om^2 - om_ce.^2))*(ee_mag.^2 - abs(producte / om).^2 - (2.0*om_ce / om)*imag(conj(ee_vec(:,1).*ee_vec(:,2))));
+
+
+
+
+
+
