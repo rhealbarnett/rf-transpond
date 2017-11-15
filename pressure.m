8dc2c47258875ab370d5dc5a21dfec2d275156e7
rhealbarnett <rhealbarnett@gmail.com>
2017-11-15 15:07:56 -0500

    changed Ne and Ni variable names to N0e and N0i

--- pressure.m
+++ pressure.m
@@ -4,18 +4,18 @@
 % rlbarnett c3149416 141117                %
 %------------------------------------------%
 
-gradNe = zeros(npts,1);
-gradNi = zeros(npts,1);
+gradN0e = zeros(npts,1);
+gradN0i = zeros(npts,1);
 
 for ii=2:(npts-1)
     
-    gradNe(ii) = (Ne(ii-1) - Ne(ii+1))/(2.0*dx);
-    gradNi(ii) = (Ni(ii-1) - Ni(ii+1))/(2.0*dx);
+    gradN0e(ii) = (N0e(ii-1) - N0e(ii+1))/(2.0*dx);
+    gradN0i(ii) = (N0i(ii-1) - N0i(ii+1))/(2.0*dx);
     
 end
 
-gradNe(1) = (-3.0*Ne(1) + 4.0*Ne(2) - Ne(3))/(2.0*dx);
-gradNe(npts) = (3.0*Ne(npts) - 4.0*Ne(npts-1) + Ne(npts-2))/(2.0*dx);
+gradN0e(1) = (-3.0*N0e(1) + 4.0*N0e(2) - N0e(3))/(2.0*dx);
+gradN0e(npts) = (3.0*N0e(npts) - 4.0*N0e(npts-1) + N0e(npts-2))/(2.0*dx);
 
-gradNi(1) = (-3.0*Ni(1) + 4.0*Ni(2) - Ni(3))/(2.0*dx);
-gradNi(npts) = (3.0*Ni(npts) - 4.0*Ni(npts-1) + Ni(npts-2))/(2.0*dx);
+gradN0i(1) = (-3.0*N0i(1) + 4.0*N0i(2) - N0i(3))/(2.0*dx);
+gradN0i(npts) = (3.0*N0i(npts) - 4.0*N0i(npts-1) + N0i(npts-2))/(2.0*dx);
