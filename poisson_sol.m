867f040cb0285108542fa1a876de7aedcc14e9ff
rhealbarnett <rhealbarnett@gmail.com>
2017-11-16 14:53:00 -0500

    fixed potential calculation because it's a scalar duh

--- poisson_sol.m
+++ poisson_sol.m
@@ -4,90 +4,36 @@
 % rlbarnett c3149416 191017                %
 %------------------------------------------%
 
-%function static_pot = poisson_sol(Ne, Ni, lamby, lambz, e, eps0, dx, npts)
+% function static_pot = poisson_sol(Ne, Nh, Nd, lamby, lambz, e, eps0, dx, npts);
 
-%--
-% initialise coefficient matrix and right hand side vector
-coeff_mat = zeros(npts,3.0*npts);
+coeff_mat = zeros(npts,npts);
 rhs = zeros(npts,1);
 
-%--
-% calculate charge density
+mult = dx^2*(lamby^2 + lambz^2) - 2.0;
 rho = (dx^2*e/eps0)*(N0e - N0i);
-
-%--
-% counter
-kk = 1;
 
 for ii=1:npts
     
-    iix = kk;
-    iiy = kk + 1;
-    iiz = kk + 2;
-    iixm = iix - 3;
-    iiym = iiy - 3;
-    iizm = iiz - 3;
-    iixp = iix + 3;
-    iiyp = iiy + 3;
-    iizp = iiz + 3;
+    iip = ii + 1;
+    iim = ii - 1;
     
-    %--
-    % rolling boundary conditions
-    if ((iixp)) > 3.0*npts
-        iixp = iixp - 3.0*npts;
-        iiyp = iiyp - 3.0*npts;
-        iizp = iizp - 3.0*npts;
+    if iip > npts
+        iip = iip - npts;
     end
-    if ((iixm) & (iiym) & (iizm)) <= 0
-        iixm = 3.0*npts + iixm;
-        iiym = 3.0*npts + iiym;
-        iizm = 3.0*npts + iizm;
+    if iim <= 0
+        iim = npts + iim;
     end
     
-    %--
-    % populate coefficient matrix
-    coeff_mat(ii,iixm) = 1.0;
-    coeff_mat(ii,iiym) = 0.0;
-    coeff_mat(ii,iizm) = 0.0;
-    coeff_mat(ii,iix) = -2.0;
-    coeff_mat(ii,iiy) = dx^2*lamby^2;
-    coeff_mat(ii,iiz) = dx^2*lambz^2;
-    coeff_mat(ii,iixp) = 1.0;
-    coeff_mat(ii,iiyp) = 0.0;
-    coeff_mat(ii,iizp) = 0.0;
+    coeff_mat(ii,ii) = mult;
+    coeff_mat(ii,iim) = 1.0;
+    coeff_mat(ii,iip) = 1.0;
     
-    %--
-    % populate right hand side vector
     rhs(ii) = rho(ii);
-    
-    kk = kk + 3;
     
 end
 
-%--
-% set simple PEC boundaries for now
-rhs(1) = 0.0;
-rhs(npts) = 0.0;
-
-%--
-% reduce matrix size
 coeff_mat = sparse(coeff_mat);
 
-%--
-% solve Ax = b
 static_pot = coeff_mat\rhs;
 
-static_potx = static_pot(1:3:3*npts);
-static_poty = static_pot(2:3:3*npts);
-static_potz = static_pot(3:3:3*npts);
-
-%end
-    
-    
-
-
-
-
-
-
-
+% end
