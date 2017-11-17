4218e64c6d2c5e8a8a0f8abc11804b1756172137
rhealbarnett <rhealbarnett@gmail.com>
2017-11-17 15:11:18 -0500

    change ODE solver from ode45 to bvp4c

--- cont_slow.m
+++ cont_slow.m
@@ -9,18 +9,22 @@ Bv_para = rot(3,1)*gradN0ex + (3.0 / 2.0)*(rot(3,2)*lamby + rot(3,3)*lambz)*N0e;
 Cv_para = (vd_perp1e*rot(1,1) + vd_perp2e*rot(2,1)).*gradN0ex + (vd_perp1e*(rot(1,2)*lamby + rot(1,3)*lambz)...
     + vd_perp2e*(rot(2,2)*lamby + rot(2,3)*lambz)).*N0e + (1.0 / 2.0)*real(gradient(conj(N1e).*v1e,dx));
 
-bound = vt;
+solinit = bvpinit(xax, [0]);
 
-[x,v_para] = ode45(@(x,v_para) myode(x, v_para, xax, Av_para, Bv_para, Cv_para), xax, bound);
+sol = bvp4c(@(x,v_para)ode(x,v_para,Av_para, Bv_para, Cv_para),@(ya,yb)bcs(ya,yb,vt),solinit);
 
-function dv_paradx = myode(x, v_para, xax, Av_para, Bv_para, Cv_para)
+function dv_paradx = ode(x,v_para, Av_para, Bv_para, Cv_para)
+    
+    dv_paradx = -(Bv_para(1)*v_para + Cv_para(1))/Av_para(1);
+%     dydx = [y(2); -abs(y(1))];
+    
+end
 
-    Av_para = interp1(xax,Av_para,x);
-    Bv_para = interp1(xax,Bv_para,x);
-    Cv_para = interp1(xax,Cv_para,x);
+function res = bcs(ya,yb,vt)
 
-    dv_paradx = -(Bv_para*v_para + Cv_para)./Av_para;
-
+    res = [ya - vt];%; yb(1) - 0.3679];
+%     res = [ya(1); yb(1) + 2];
+    
 end
 
 
