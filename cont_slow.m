52caaad0f343437a7eef6695de2ab48628b87ca2
rhealbarnett <rhealbarnett@gmail.com>
2017-11-17 17:02:02 -0500

    generalise coefficient calculations as functions of x

--- cont_slow.m
+++ cont_slow.m
@@ -13,12 +13,38 @@ solinit = bvpinit(xax, [0]);
 
 sol = bvp4c(@(x,v_para)ode(x,v_para,Av_para, Bv_para, Cv_para),@(ya,yb)bcs(ya,yb,vt),solinit);
 
-function dv_paradx = ode(x,v_para, Av_para, Bv_para, Cv_para)
+
+
+function [ans] = get_N0e(x)
+
+    ans = interp1(xax,N0e,x);
+
+end
+
+function [ans] = get_gradN0ex(x,dx)
+
+    ans = gradient(@get_N0e,dx);
+
+end
+
+function [ans] = B(x)
+
+    ans = rot(3,1)*get_gradN0ex(x) + (3.0 / 2.0)*(rot(3,2)*lamby + rot(3,3)*lambz)*get_N0e(x);
+
+end
+
+function dydx = ode(x,y)
     
-    dv_paradx = -(Bv_para(1)*v_para + Cv_para(1))/Av_para(1);
-%     dydx = [y(2); -abs(y(1))];
+    dydx = -(B(x)*y + C(x))/A(x);
     
 end
+
+% function dv_paradx = ode(x,v_para, Av_para, Bv_para, Cv_para)
+%     
+%     dv_paradx = -(Bv_para*v_para + Cv_para)/Av_para;
+% %     dydx = [y(2); -abs(y(1))];
+%     
+% end
 
 function res = bcs(ya,yb,vt)
 
