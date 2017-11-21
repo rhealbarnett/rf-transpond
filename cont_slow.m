3e6c78406d6270f4cdd4c508bc8af5b3b583f224
rhealbarnett <rhealbarnett@gmail.com>
2017-11-21 15:55:59 -0500

    edited script to call dydx function and bvp4c solve from separate script

--- cont_slow.m
+++ cont_slow.m
@@ -1,5 +1,5 @@
 %------------------------------------------%
-% ODE test solve                           %
+% ODE solve for parallel velocity          %
 % DVE 2015 eq 23                           %
 % rlbarnett c3149416 151117                %
 %------------------------------------------%
@@ -16,7 +16,7 @@ get_N0e = @(x) interp1(xax,N0e,x);
 
 %--
 % calculate A coefficient 
-A = @(x) get_N0e(x)*rot(3,1);
+A = @(x) get_N0e(x).*rot(3,1);
 
 %--
 % interpolate grad*N0e over arbitrary x range
@@ -24,7 +24,7 @@ get_gradN0ex = @(x) gradient(get_N0e(x),dx);
 
 %--
 % calculate B coefficient
-B = @(x) get_gradN0ex(x)*rot(3,1) + get_N0e(x)*(3.0 / 2.0)*(rot(3,2)*lamby + rot(3,3)*lambz);
+B = @(x) get_gradN0ex(x).*rot(3,1) + get_N0e(x).*(3.0 / 2.0)*(rot(3,2)*lamby + rot(3,3)*lambz);
 
 %--
 % interpolate N1e, v1e and perpendicular drift velocities over arbitrary x
@@ -36,20 +36,29 @@ get_vdperp2e = @(x) interp1(xax,vd_perp2e,x);
 
 %--
 % calculate C coefficient
-C = @(x) get_gradN0ex(x)*(get_vdperp1e(x)*rot(1,1) + get_vdperp2e(x)*rot(2,1)) + get_N0e(x)*(get_vdperp1e(x)*(rot(1,2)*lamby + rot(1,3)*lambz)...
-        + get_vdperp2e(x)*(rot(2,2)*lamby + rot(2,3)*lambz)) + (1.0 / 2.0)*real(gradient(conj(get_N1e(x))*get_v1e(x),dx));
+C = @(x) get_gradN0ex(x).*(get_vdperp1e(x).*rot(1,1) + get_vdperp2e(x).*rot(2,1)) + get_N0e(x).*(get_vdperp1e(x).*(rot(1,2)*lamby +...
+    rot(1,3)*lambz) + get_vdperp2e(x).*(rot(2,2)*lamby + rot(2,3)*lambz)) + (1.0 / 2.0).*real(gradient(conj(get_N1e(x)).*get_v1e(x),dx));
 
 %--
-% define the differential equation and boundary conditions as an anonymous
+% define the boundary conditions as anonymous
 % functions
-dydx = @(x,y) -(B(x)*y + C(x))/A(x);
 bound = @(ya,yb) yb - vt;
 
 %--
-% initial guess for boundary value problem solution and call to bvp4c
-% function
-solinit = bvpinit(xax, [0.2]);
-sol = bvp4c(dydx,bound,solinit);
+% initial guess for boundary value problem solution
+solinit = bvpinit(xax,xmax);
+
+%--
+% call to ode_solve, inputs A(x), B(x) & C(x), bound and solinit
+% outputs sol
+ode_solve;
+
+%--
+% label the solutions for use in later codes
+v_parae = sol.y;
+gradv_paraex = sol.yp;
+
+clear A B C bound
 
 
 
