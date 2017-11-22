e0cf39771fe90287bcf7bbae0547c4d78ef1760b
rhealbarnett <rhealbarnett@gmail.com>
2017-11-22 09:18:33 -0500

    edited expression for static E field in C coefficient for equation 24 -- did not fix

--- eqofmot_slow.m
+++ eqofmot_slow.m
@@ -17,11 +17,11 @@ v_paraesq = v_parae.^2;
 get_vparaesq = @(x) interp1(xax,v_paraesq,x);
 
 %--
-% A coefficient -- rename???? 
+% A coefficient
 A = @(x) (get_vparaesq(x) - vt^2).*rot(3,1) + get_vparae(x).*(get_vdperp1e(x).*rot(1,1) + get_vdperp2e(x).*rot(2,1));
 
 %--
-% B coefficient -- rename????
+% B coefficient
 B = @(x) (get_vparaesq(x) - vt^2).*(rot(3,2)*lamby + rot(3,3)*lambz) + get_vparae(x).*(get_vdperp1e(x).*(rot(1,2)*lamby +...
     rot(1,3)*lambz) + get_vdperp2e(x).*(rot(2,2)*lamby + rot(2,3)*lambz));
 
@@ -30,13 +30,15 @@ B = @(x) (get_vparaesq(x) - vt^2).*(rot(3,2)*lamby + rot(3,3)*lambz) + get_vpara
 get_gradvparaex = @(x) interp1(xax,gradv_paraex,x);
 get_staticpot = @(x) interp1(xax,static_pot,x);
 get_staticex = @(x) interp1(xax,static_ex,x);
+get_staticey = @(x) interp1(xax,static_ey,x);
+get_staticez = @(x) interp1(xax,static_ez,x);
 get_apondex = @(x) interp1(xax,a_pondex,x);
 
 %--
-% C coefficient -- rename????
+% C coefficient
 C = @(x) -get_vdperp1e(x).*(rot(1,1).*get_gradvparaex(x) + (get_vparae(x)./2.0)*(rot(1,2)*lamby + rot(1,3)*lambz)) -...
     get_vdperp2e(x).*(rot(2,1).*get_gradvparaex(x) + (get_vparae(x)./2.0)*(rot(2,2)*lamby + rot(2,3)*lambz)) +...
-    (abs(e)/me)*(rot(3,1).*get_staticex(x) - ((rot(3,2)*lamby + rot(3,3)*lambz).*get_staticpot(x)));% + rot(3,1).*get_apondex(x) +...
+    (abs(e)/me)*(rot(3,1).*get_staticex(x) + rot(3,2).*get_staticey(x) + rot(3,3).*get_staticez(x));% + rot(3,1).*get_apondex(x);% +...
     %(get_vparae(x)./(2.0.*get_N0e(x))).*real(gradient(conj(get_N1e(x)).*get_v1e(x),dx));
 
 %--
