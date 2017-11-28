2ceb8ae2904be806374006a3d1912735ead508e5
rhealbarnett <rhealbarnett@gmail.com>
2017-11-28 16:08:58 -0500

    uncommented sections of C coefficient that were forgotten after testing -- still solves

--- eqofmot_slow.m
+++ eqofmot_slow.m
@@ -40,8 +40,8 @@ get_apondez = @(x) interp1(xax,a_pondez,x);
 % C coefficient
 C = @(x) -get_vdperp1e(x).*(rot(1,1).*get_gradvparaex(x) + (get_vparae(x)./2.0)*(rot(1,2)*lamby + rot(1,3)*lambz)) -...
     get_vdperp2e(x).*(rot(2,1).*get_gradvparaex(x) + (get_vparae(x)./2.0)*(rot(2,2)*lamby + rot(2,3)*lambz)) +...
-    ((e)/me)*(rot(3,1).*get_staticex(x));% + rot(3,2).*get_staticey(x) + rot(3,3).*get_staticez(x)) + rot(3,1).*get_apondex(x) +...
-%     (get_vparae(x)./(2.0.*get_N0e(x))).*real(gradient(conj(get_N1e(x)).*get_v1e(x),dx));
+    ((e)/me)*(rot(3,1).*get_staticex(x) + rot(3,2).*get_staticey(x) + rot(3,3).*get_staticez(x)) + rot(3,1).*get_apondex(x) +...
+    (get_vparae(x)./(2.0.*get_N0e(x))).*real(gradient(conj(get_N1e(x)).*get_v1e(x),dx));
 
 %--
 % bounday condition 
@@ -49,7 +49,7 @@ bound = @(ya,yb) yb - log(Nmax);
 
 %--
 % initial guess for boundary value problem solution
-test = linspace(xmin,xmax,10*npts);
+test = linspace(xmin,xmax,npts);
 solinit = bvpinit(test,log(Nmax)/2);
 
 %--
