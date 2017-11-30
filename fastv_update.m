25d1995462e1ac4befc0220ad40f5bf74bb99bfa
rhealbarnett <rhealbarnett@gmail.com>
2017-11-30 14:30:56 -0500

    added ion calcs

--- fastv_update.m
+++ fastv_update.m
@@ -4,16 +4,25 @@
 % rlbarnett c3149416 281117                %
 %------------------------------------------%
 
-denom = 1.0 ./ (om^2 - om_ce.^2);
-cross_term = zeros(npts,3);
-dot_term = zeros(npts,3);
+denome = 1.0 ./ (om^2 - om_ce.^2);
+denomi = 1.0 ./ (om^2 - om_cimag.^2);
+om_ci_vec = sqrt(om_cd_vec.^2 + om_ch_vec.^2);
+ei_vec = sqrt(eh_vec.^2 + ed_vec.^2);
+producti = sqrt(producth.^2 + productd.^2);
+cross_terme = zeros(npts,3);
+dot_terme = zeros(npts,3);
+cross_termi = zeros(npts,3);
+dot_termi = zeros(npts,3);
 
 for ii=1:npts
     
-    cross_term(ii,:) = cross(ee_vec(ii,:),Bvec);
-    dot_term(ii,:) = (1i / om)*producte(ii)*om_ce_vec;
+    cross_terme(ii,:) = cross(ee_vec(ii,:),Bvec);
+    dot_terme(ii,:) = (1i / om)*producte(ii)*om_ce_vec;
+    cross_termi(ii,:) = cross(ei_vec(ii,:),Bvec);
+    dot_termi(ii,:) = (1i / om)*producti(ii)*om_ci_vec;
     
 end
     
 
-v1e = denom*(-1i*om.*ee_vec + cross_term + dot_term);
+v1e = denome*(-1i*om.*ee_vec + cross_terme + dot_terme);
+v1i = denomi*(-1i*om.*ei_vec + cross_termi + dot_termi);
