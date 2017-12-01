80f09dfb69623a4ef91483220c61dbac56776a34
rhealbarnett <rhealbarnett@gmail.com>
2017-12-01 13:44:00 -0500

    removed commented out FD scheme (was replaced with the gradient function)

--- static_e.m
+++ static_e.m
@@ -7,13 +7,4 @@
 static_ey = -lamby*static_pot;
 static_ez = -lambz*static_pot;
 
-% static_ex = zeros(npts,1);
-
-% for ii=2:(npts-1)
-%     static_ex(ii) = (static_pot(ii-1) - static_pot(ii+1))/(2.0*dx);
-% end
-
-% static_ex(1) = (-3.0*static_pot(1) + 4.0*static_pot(2) - static_pot(3))/(2.0*dx);
-% static_ex(npts) = (3.0*static_pot(npts) - 4.0*static_pot(npts-1) + static_pot(npts-2))/(2.0*dx);
-
 static_ex = -gradient(static_pot,dx);
