589137246c68950e2db2ad442d200866d80d61d1
rhealbarnett <rhealbarnett@gmail.com>
2018-01-11 17:37:00 -0500

    separated dielectric tensor calculations into different script

---
+++ dielec_tens.m
@@ -0,0 +1,32 @@
+%-----------------------------------------%
+% Populate cold plasma dielectric tensor  %
+% arbitrary species                       %
+% rlbarnett c3149416, 121217              %
+%-----------------------------------------%
+
+% function ans = cpdt(npts,N,B0,om,q,m,sign,eps0)
+% 
+%     om_c = q*B0./m;
+%     om_p = sqrt(q^2*N./(eps0*m));
+%     
+% end
+
+cpdt = zeros(3,3,npts);
+    
+for nn=1:npts
+    s = 1.0 - om_pe(nn)^2/(om^2 - om_ce(nn)^2) - om_pd(nn)^2/(om^2 - om_cd(nn)^2) - om_ph(nn)^2/(om^2 - om_ch(nn)^2);
+    d = om_ce(nn)*om_pe(nn)^2/(om*(om^2 - om_ce(nn)^2)) + om_cd(nn)*om_pd(nn)^2/(om*(om^2 - om_cd(nn)^2)) +...
+        om_ch(nn)*om_ph(nn)^2/(om*(om^2 - om_ch(nn)^2));
+    p = 1.0 - om_pe(nn)^2/om^2 - om_pd(nn)^2/om^2 - om_ph(nn)^2/om^2;
+
+    %--
+    % cold plasma delectric tensor
+    cpdt(1,1,nn) = s;
+    cpdt(2,2,nn) = s;
+    cpdt(1,2,nn) = -1i*d;
+    cpdt(2,1,nn) = 1i*d;
+    cpdt(3,3,nn) = p;
+    
+    cpdt(:,:,nn) = rot'*cpdt(:,:,nn)*rot;
+   
+end
