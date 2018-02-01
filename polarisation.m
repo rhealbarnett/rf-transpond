17ef786fc1bcb2911e6882b7f01b13a299822be5
rhealbarnett <rhealbarnett@gmail.com>
2018-02-01 16:42:41 -0500

    polarisation calculated analytically from Brambilla equations 22.13 and 22.14

--- polarisation.m
+++ polarisation.m
@@ -1,178 +1,120 @@
 %-----------------------------------------%
-% Calculate possible wave polarisation    %
-% rlbarnett c3149416, 100118              %
+% Calculate analytical wave polarisation  %
+% rlbarnett c3149416, 010218              %
 %-----------------------------------------%
 
-%--
-% initialise arrays for eigenvectors and eigenvalues
-eigvec = zeros(3,npts,4);
-eigval = zeros(1,npts,4);
-exezkp11 = zeros(1,npts);
-exezkp12 = zeros(1,npts);
-exezkp21 = zeros(1,npts);
-exezkp22 = zeros(1,npts);
+exey_fast = -1i*(d_arr./(kpara^2 - s_arr.^2));
 
-for kk=1:4
-    for ii=1:npts
+ezey_fast11 = -1i*kpara*kp11.*(d_arr./(p_arr.*(kpara^2 - s_arr)));
+ezey_fast12 = -1i*kpara*kp12.*(d_arr./(p_arr.*(kpara^2 - s_arr)));
+ezey_fast21 = -1i*kpara*kp21.*(d_arr./(p_arr.*(kpara^2 - s_arr)));
+ezey_fast22 = -1i*kpara*kp22.*(d_arr./(p_arr.*(kpara^2 - s_arr)));
 
-        %--
-        % recalculate wave equation matrix again 
-        % seems super unnessesary to have this again, probably can work
-        % something better than this
-        wave_eq = a - we_rhs(:,:,ii);
+exez_slow11 = (kp11.*kpara)./(kpara^2 - s_arr);
+exez_slow12 = (kp12.*kpara)./(kpara^2 - s_arr);
+exez_slow21 = (kp21.*kpara)./(kpara^2 - s_arr);
+exez_slow22 = (kp22.*kpara)./(kpara^2 - s_arr);
 
-        %--
-        % substitute kx values into the matrix
-%         subbed = subs(wave_eq,kx,kx_arr(ii,kk));
-        subbed = subs(wave_eq,kperp,sf_arr(ii,kk));
-
-        %--
-        % calculate eigenvalues and eigenvectors
-        subbed = eval(subbed);
-        [vecs,vals] = eig(subbed);
-
-        %--
-        % find the index of the minimum eigenvalue
-        mineig = find((diag(vals)) == min((diag(vals))));
-
-        %--
-        % take eigenvector associated with the min eigenvalue
-        eigvec(:,ii,kk) = vecs(:,mineig);
-        eigval(1,ii,kk) = vals(mineig,1);
-        
-        %--
-        % calculate polarisation ratios
-        exezkp11(ii) = eigvec(1,ii,1)/eigvec(3,ii,1);
-        exezkp12(ii) = eigvec(1,ii,2)/eigvec(3,ii,2);
-        exezkp21(ii) = eigvec(1,ii,3)/eigvec(3,ii,3);
-        exezkp22(ii) = eigvec(1,ii,4)/eigvec(3,ii,4);
-
-    end
-end
+eyez_slow11 = 1i*(kpara*d_arr)./(kp11.*(kpara^2 - s_arr));
+eyez_slow12 = 1i*(kpara*d_arr)./(kp12.*(kpara^2 - s_arr));
+eyez_slow21 = 1i*(kpara*d_arr)./(kp21.*(kpara^2 - s_arr));
+eyez_slow22 = 1i*(kpara*d_arr)./(kp22.*(kpara^2 - s_arr));
 
 %%
-%--
-% plot eigenvectors (ie polarisation)
-figure(10)
-set(gcf,'Position',[0   536   824   419])
-suptitle('kp1')
 
+figure(20)
+set(gcf,'Position',[4 68 857 886])
+suptitle('Fast wave polarisation')
 subplot(3,1,1)
-plot(log10(N0),real(eigvec(1,:,1)),'.')
+plot(log10(N0(1:imme)),real(exey_fast(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(1,:,1)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ex]', 'Im[Ex]')
-set(gca, 'XTickLabel', [])
+
+plot(log10(N0(1:imme)),imag(exey_fast(1:imme)),'.r')
+xlim([min(log10(N0)) log10(N0(imme))])
+xlabel('log$_{10}|$N$_0|$','Fontsize',16)
+ylabel('E$_x$/E$_y$','Fontsize',16)
+legend('Re[E$_x$/E$_y$]','Im[E$_x$/E$_y$]')
 hold off
 
 subplot(3,1,2)
-plot(log10(N0),real(eigvec(2,:,1)),'.')
+plot(log10(N0(1:imme)),real(ezey_fast11(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(2,:,1)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ey]', 'Im[Ey]')
-set(gca, 'XTickLabel', [])
+
+plot(log10(N0(1:imme)),imag(ezey_fast11(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(ezey_fast12(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(ezey_fast12(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
+xlabel('log$_{10}|$N$_0|$','Fontsize',16)
+ylabel('E$_z$/E$_y$','Fontsize',16)
+legend('Re[E$_z$/E$_y$]$_{\perp1,1}$','Im[E$_z$/E$_y$]$_{\perp1,1}$','Re[E$_z$/E$_y$]$_{\perp1,2}$','Im[E$_z$/E$_y$]$_{\perp1,2}$')
 hold off
 
 subplot(3,1,3)
-plot(log10(N0),real(eigvec(3,:,1)),'.')
+plot(log10(N0(1:imme)),real(ezey_fast21(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(3,:,1)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
+
+plot(log10(N0(1:imme)),imag(ezey_fast21(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(ezey_fast22(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(ezey_fast22(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
 xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-legend('Re[Ez]', 'Im[Ez]')
+ylabel('E$_z$/E$_y$','Fontsize',16)
+legend('Re[E$_z$/E$_y$]$_{\perp2,1}$','Im[E$_z$/E$_y$]$_{\perp2,1}$','Re[E$_z$/E$_y$]$_{\perp2,2}$','Im[E$_z$/E$_y$]$_{\perp2,2}$')
 hold off
 
-figure(11)
-set(gcf,'Position',[859   536   824   419])
-suptitle('kp1')
-
-subplot(3,1,1)
-plot(log10(N0),real(eigvec(1,:,2)),'.')
+figure(21)
+set(gcf,'Position',[865 69 813 886])
+suptitle('Slow wave polarisation')
+subplot(4,1,1)
+plot(log10(N0(1:imme)),real(exez_slow11(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(1,:,2)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ex]', 'Im[Ex]')
-set(gca, 'XTickLabel', [])
-hold off
 
-subplot(3,1,2)
-plot(log10(N0),real(eigvec(2,:,2)),'.')
-hold on
-plot(log10(N0),imag(eigvec(2,:,2)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ey]', 'Im[Ey]')
-set(gca, 'XTickLabel', [])
-hold off
-
-subplot(3,1,3)
-plot(log10(N0),real(eigvec(3,:,2)),'.')
-hold on
-plot(log10(N0),imag(eigvec(3,:,2)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
+plot(log10(N0(1:imme)),imag(exez_slow11(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(exez_slow12(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(exez_slow12(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
 xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-legend('Re[Ez]', 'Im[Ez]')
+ylabel('E$_x$/E$_z$','Fontsize',16)
+legend('Re[E$_x$/E$_z$]$_{\perp1,1}$','Im[E$_x$/E$_z$]$_{\perp1,1}$','Re[E$_x$/E$_z$]$_{\perp1,2}$','Im[E$_x$/E$_z$]$_{\perp1,2}$')
 hold off
 
-figure(12)
-set(gcf,'Position',[6    60   824   419])
-suptitle('kp2')
-
-subplot(3,1,1)
-plot(log10(N0),real(eigvec(1,:,3)),'.')
+subplot(4,1,2)
+plot(log10(N0(1:imme)),real(exez_slow21(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(1,:,3)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ex]', 'Im[Ex]')
-set(gca, 'XTickLabel', [])
-hold off
 
-subplot(3,1,2)
-plot(log10(N0),real(eigvec(2,:,3)),'.')
-hold on
-plot(log10(N0),imag(eigvec(2,:,3)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ey]', 'Im[Ey]')
-set(gca, 'XTickLabel', [])
-hold off
-
-subplot(3,1,3)
-plot(log10(N0),real(eigvec(3,:,3)),'.')
-hold on
-plot(log10(N0),imag(eigvec(3,:,3)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
+plot(log10(N0(1:imme)),imag(exez_slow21(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(exez_slow22(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(exez_slow22(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
 xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-legend('Re[Ez]', 'Im[Ez]')
+ylabel('E$_x$/E$_z$','Fontsize',16)
+legend('Re[E$_x$/E$_z$]$_{\perp2,1}$','Im[E$_x$/E$_z$]$_{\perp2,1}$','Re[E$_x$/E$_z$]$_{\perp2,2}$','Im[E$_x$/E$_z$]$_{\perp2,2}$')
 hold off
 
-figure(13)
-set(gcf,'Position',[841 71 835 420])
-suptitle('kp2')
-
-subplot(3,1,1)
-plot(log10(N0),real(eigvec(1,:,4)),'.')
+subplot(4,1,3)
+plot(log10(N0(1:imme)),real(eyez_slow11(1:imme)),'.k')
 hold on
-plot(log10(N0),imag(eigvec(1,:,4)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ex]', 'Im[Ex]')
-set(gca, 'XTickLabel', [])
-hold off
 
-subplot(3,1,2)
-plot(log10(N0),real(eigvec(2,:,4)),'.')
-hold on
-plot(log10(N0),imag(eigvec(2,:,4)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
-legend('Re[Ey]', 'Im[Ey]')
-set(gca, 'XTickLabel', [])
-hold off
-
-subplot(3,1,3)
-plot(log10(N0),real(eigvec(3,:,4)),'.')
-hold on
-plot(log10(N0),imag(eigvec(3,:,4)),'.')
-ylabel('Amplitude (Vm$^{-1}$)')
+plot(log10(N0(1:imme)),imag(eyez_slow11(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(eyez_slow12(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(eyez_slow12(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
 xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-legend('Re[Ez]', 'Im[Ez]')
+ylabel('E$_y$/E$_z$','Fontsize',16)
+legend('Re[E$_y$/E$_z$]$_{\perp1,1}$','Im[E$_y$/E$_z$]$_{\perp1,1}$','Re[E$_y$/E$_z$]$_{\perp1,2}$','Im[E$_y$/E$_z$]$_{\perp1,2}$')
 hold off
+
+subplot(4,1,4)
+plot(log10(N0(1:imme)),real(eyez_slow21(1:imme)),'.k')
+hold on
+
+plot(log10(N0(1:imme)),imag(eyez_slow21(1:imme)),'.r')
+plot(log10(N0(1:imme)),real(eyez_slow22(1:imme)),'dk','MarkerSize',3)
+plot(log10(N0(1:imme)),imag(eyez_slow22(1:imme)),'dr','MarkerSize',3)
+xlim([min(log10(N0)) log10(N0(imme))])
+xlabel('log$_{10}|$N$_0|$','Fontsize',16)
+ylabel('E$_y$/E$_z$','Fontsize',16)
+legend('Re[E$_y$/E$_z$]$_{\perp2,1}$','Im[E$_y$/E$_z$]$_{\perp2,1}$','Re[E$_y$/E$_z$]$_{\perp2,2}$','Im[E$_y$/E$_z$]$_{\perp2,2}$')
+hold off
+
+
