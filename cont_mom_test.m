88bd56a123ea862c4a702fce0f4ddf25c4a29e86
rhealbarnett <rhealbarnett@gmail.com>
2018-03-17 01:05:24 -0400

    comparison test with comsol

--- cont_mom_test.m
+++ cont_mom_test.m
@@ -11,24 +11,24 @@ c0 = 3.0e8;
 %--
 % spatial domain
 xmin = 0;
-xmax = 1.5;
-% xax = linspace(xmin,xmax,256);
-% npts = length(xax);
-% dx = (xmax - xmin)/(npts-1);
-dx = 0.0013378;
-xax = xmin:dx:xmax;
+xmax = 2;
+xax = linspace(xmin,xmax,256);
 npts = length(xax);
+dx = (xmax - xmin)/(npts-1);
+% dx = 0.0013378;
+% xax = xmin:dx:xmax;
+% npts = length(xax);
 
 %--
 % temporal domain
 tmin = 0;
-% dt = 0.99*dx/c0;
-% tmax = 1.0e6*dt;
-% nmax = tmax/dt;
-% tax = linspace(tmin,tmax,nmax);
-% tol = 1.0e-5;
-dt = 0.99*dx;
-nmax = 756;
+dt = 0.99*dx/c0;
+tmax = 1.0e7*dt;
+nmax = tmax/dt;
+tax = linspace(tmin,tmax,nmax);
+tol = 1.0e-5;
+% dt = 0.99*dx;
+% nmax = 756;
 
 %--
 % constants
@@ -37,8 +37,8 @@ m = 1.67e-27;
 
 %--
 % parameters
-Te = 10e3;
-Ti = 5e3;
+Te = 5.0;
+Ti = 5.0;
 T = Te + Ti;
 cs = sqrt((Te + Ti)*e/m);
 
@@ -67,21 +67,23 @@ hold off
 % up wind differencing scheme
 
 n = zeros(1,npts);
-n(1) = 1.0e19;
+n(1,:) = 1.0e19;
+% n(1) = 1.0e19;
 
-% for ii=1:nmax
-%     n_old = n;
-%     for jj=2:npts-1
-%         n(1,jj) = n(1,jj) - vx(1,jj)*(dt/dx)*(n(1,jj) - n(1,jj-1)) - n(1,jj)*dt*dvx(1,jj);
-%     end
-%     n(1,npts) = n(1,npts-1);
+for ii=1:nmax
+    n_old = n;
+    for jj=2:npts-1
+        n(1,jj) = n(1,jj) - vx(1,jj)*(dt/dx)*(n(1,jj) - n(1,jj-1)) - n(1,jj)*(dt)*dvx(1,jj);%/dx)*(vx(1,jj) - vx(1,jj-1));
+    end
+    n(1,1) = 1.0e19;
+    n(1,end) = n(1,end-1);
 %     if abs(rms(n_old)-rms(n))<tol
 %         break
 %         ii
 %     else
 %         continue
 %     end
-% end
+end
 
 
 %--
@@ -99,11 +101,12 @@ clear vx dvx n
 
 %--
 % density
-Nmax = 20;
+Nmax = 19;
 Nmin = 16;
 m = (Nmax - Nmin) ./ (xmax - xmin);
 n = 10.^(-m*xax + Nmax);
-% n = -0.5e19*(xax)+1.05e19;
+% n = 1.0e19*ones(1,npts);
+n = 0.5e19*(xax)+1.05e19;
 dnx = gradient(n,xax);
 
 %--
@@ -120,12 +123,13 @@ hold off
 % up wind differencing scheme
 % CONSERVATIVE FORM ie (all partials) dv/dt + d(v^2/2)/dx = ...
 
-vx = zeros(1,npts);
-% vx(1) = 0;
-% vx(npts) = cs;
+vx = (cs/2)*ones(1,npts);
+% vx = zeros(1,npts);
+% vx(1,1) = 0;
+% vx(1,end) = cs;
 % vx = sin(xax);
-a = find(xax<=0.5);
-vx(a(end):end) = 1.0;
+% a = find(xax<=0.5);
+% vx(a(end):end) = 1.0;
 vx_new = zeros(1,npts);
 
 figure(5)
@@ -134,15 +138,18 @@ hold on
 
 for ii=1:nmax
     for jj=2:npts-1
-        vx_new(1,jj) = (1./2.)*(vx(1,jj-1) + vx(1,jj+1)) - (dt/(2.0*dx))*((vx(1,jj+1)^2)/2. - (vx(1,jj-1)^2)/2.);% - ((T*e)/(n(1,jj)*m))*dt*dnx(1,jj);
+        vx(1,jj) = (1./2.)*(vx(1,jj-1) + vx(1,jj+1)) - (dt/(2.0*dx))*((vx(1,jj+1)^2)/2. - (vx(1,jj-1)^2)/2.) - ((T*e)/(n(1,jj)*m))*(dt/dx)*...
+            (n(1,jj) - n(1,jj-1));
     end
-    vx_new(1,1) = vx(1,1);
-    vx_new(1,end) = vx_new(1,end-1);
-    vx = vx_new;
-    if (ii==100) || (ii==300) || (ii==600) || (ii==756)
-        figure(5)
-        plot(xax,vx_new)
-        hold on
+    vx(1,1) = 0.0;
+    vx(1,end) = cs;
+%     vx_new(1,1) = vx(1,1);
+%     vx(1,end) = vx(1,end-1);
+%     vx = vx_new;
+%     if (ii==100) || (ii==300) || (ii==600) || (ii==756)
+%         figure(5)
+%         plot(xax,vx_new)
+%         hold on
 %     if abs(rms(vx_old)-rms(vx))<1.0e-9
 %         break
 %     elseif isnan(vx(end-1))
@@ -150,17 +157,17 @@ for ii=1:nmax
 %     else
 %         continue
 %     end
-    end
+%     end
 end
 
-figure(5)
-legend('t=0s', 't=0.13s', 't=0.4s', 't=0.8s', 't=1.0s','Location','northwest')
-
-hold off
+% figure(5)
+% legend('t=0s', 't=0.13s', 't=0.4s', 't=0.8s', 't=1.0s','Location','northwest')
+% 
+% hold off
     
 %--
 % % plot solution to compare with comsol solution
-% figure(4)
-% plot(xax,vx)
-% xlabel('Position ($m$)','Fontsize',16)
-% ylabel('Velocity ($ms^{-1}$)','Fontsize',16)
+figure(4)
+plot(xax,vx)
+xlabel('Position ($m$)','Fontsize',16)
+ylabel('Velocity ($ms^{-1}$)','Fontsize',16)
