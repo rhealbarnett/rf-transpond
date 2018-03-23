1da5b1dc904a30cd9a19474542362e7426ac0191
rhealbarnett <rhealbarnett@gmail.com>
2018-03-23 19:06:46 -0400

    edited to copy example -- not solving

--- mom_mms.m
+++ mom_mms.m
@@ -11,7 +11,7 @@
 tmin = 0;
 
 % choose dt based on smallest dx (assume CFL condition is dt/dx <= 1)
-dt = 1.e-5;
+dt = 5.e-4*0.99;
 
 % max time to run
 tmax = 1.0e4*dt;
@@ -23,10 +23,14 @@ nmax = tmax/dt;
 % spatial domain %
 %------
 xmin = 0;
-xmax = 1;
+xmax = 2.0*pi;
 
 % vary spatial step size
 dx = logspace(-1,-5,5);
+
+% coefficients for source term s = a + sin(x + ct)
+a = 1.0;
+c = 1.0;
 
 %%
 %-------------------------------------------------------------------------%
@@ -34,45 +38,68 @@ dx = logspace(-1,-5,5);
 % dU/dt = -cos(x-t); d/dx(U^2/2) = -(1/2)sin(2(t-x))                      %
 %-------------------------------------------------------------------------%
 
-for kk=1
-    kk
-    for ii=1:nmax
-        
-        % number of spatial points will depend on dx
-        npts = round((xmax-xmin)/dx(kk));
-        xax = linspace(xmin,xmax,npts);
-        
-        % initial conditions
-        vx = sin(xax);
-        
-        % boundary conditions (Dirichlet, time dependent)
-        vx(1,1) = sin(-dt*ii);
-        vx(1,end) = sin(xmax - dt*ii);
-        
-        % initialise source, error arrays
-        source = zeros(1,npts);
-        l_inf = zeros(1,npts);
-        l_sec = zeros(1,npts);
-        
-        % source boundary conditions
-        source(1,1) = - cos(ii*dt) - (1.0/2.0)*sin(2.0*(ii*dt));
-        source(1,end) = - cos(ii*dt - xmax) - (1.0/2.0)*sin(2.0*(ii*dt - xmax));
-        
-        for jj=2:npts-1
-            
-            % discretised solution
-            vx(1,jj) = (1./2.)*(vx(1,jj-1) + vx(1,jj+1)) - (dt/(2.0*dx(kk)))*((vx(1,jj+1)^2)/2 - (vx(1,jj-1)^2/2));
-            
-            % 'exact' solution
-            source(1,jj) = - cos(ii*dt - dx(kk)*jj) - (1.0/2.0)*sin(2.0*(ii*dt - dx(kk)*jj)); 
+nu = 0.05;
+dx = 0.1047;
+% for kk=length(dx)
+%     kk
+    
+% number of spatial points will depend on dx
+npts = round((xmax-xmin)/dx);
+xax = linspace(xmin,xmax,npts);
 
-        end
+vx_new = zeros(1,npts);
 
-        l_inf(1,ii) = abs(max(vx - source));
-        l_sec(1,ii) = rms(vx - source);
-        
+% initial conditions
+source = 0.0;%c*cos(xax) + (a + sin(xax)).*cos(xax) + nu*sin(xax);
+vx = sin(xax) + source;
+
+for ii=1:nmax
+
+
+    % source boundary conditions
+%         source(1,1) = - cos(ii*dt) + sin(-ii*dt)*cos(-ii*dt) + nu*sin(-ii*dt);
+    source(1,end) = c*cos(ii*dt*c + xmax) + (a + sin(xmax + c*dt*ii))*cos(xmax + c*dt*ii) + nu*sin(xmax + c*ii*dt);
+
+    % boundary conditions (Dirichlet, time dependent)
+%         vx(1,1) = sin(-dt*ii*c) + dt*source(1,1);
+%     vx(1,end) = vx(1,end) - ((dt*vx(1,end))/(2.0*dx))*(vx(1,2) - vx(1,end-1)) +...
+%             ((nu*dt)/(dx^2))*(vx(1,1) - 2.0*vx(1,end) + vx(1,end-1)) + dt*source(1,end);
+% 
+%     vx(1,1) = vx(1,end);
+
+    % initialise source, error arrays
+    l_inf = zeros(1,npts);
+    l_sec = zeros(1,npts);
+
+    % exact solution
+    ex_sol = a + sin(xax + c*dt*ii);
+
+    % calculation for second last spatial point
+%         source(1,end-1) = - cos(ii*dt - xax(end-1)) + sin(xax(end-1) - ii*dt)*cos(xax(end-1) - ii*dt) + nu*sin(xax(end-1) - ii*dt); 
+
+    % backward difference for second last spatial point (not included
+    % in loop)
+%         vx(1,end-1) = vx(1,end-1) - ((dt*vx(1,end-1))/(dx(kk)))*(vx(1,end-1) - vx(1,end-2)) +...
+%             ((nu*dt)/(dx(kk)^2))*(vx(1,end-1) - 2.0*vx(1,end-2) + vx(1,end-3)) + dt*source(1,end-1);
+
+    for jj=2:npts-1
+
+        source(1,jj) = c*cos(c*ii*dt + xax(jj)) + (a + sin(xax(jj) + ii*dt*c))*cos(xax(jj) + c*ii*dt) + nu*sin(xax(jj) + c*ii*dt); 
+
+        % discretised solution
+        vx_new(1,jj) = vx(1,jj) - ((dt*vx(1,jj))/(2.0*dx))*(vx(1,jj+1) - vx(1,jj-1)) +...
+            ((nu*dt)/(dx^2))*(vx(1,jj+1) - 2.0*vx(1,jj) + vx(1,jj-1)) + dt*source(1,jj);
     end
+
+    l_inf(1,ii) = abs(max(vx - ex_sol));
+    l_sec(1,ii) = rms(vx - ex_sol);
+
+    vx_new(1,end) = vx(1,end) - ((dt*vx(1,end))/(2.0*dx))*(vx(1,2) - vx(1,end-1)) +...
+            ((nu*dt)/(dx^2))*(vx(1,2) - 2.0*vx(1,end) + vx(1,end-1)) + dt*source(1,end);
+    vx_new(1,1) = vx_new(1,end);    
+    vx = vx_new;
 end
+% end
 
 
 %%
@@ -81,8 +108,8 @@ end
 %------
 
 figure(1)
-plot(xax, vx)
+plot(xax, vx, '*k')
 hold on
-plot(xax, source)
-legend('vx', 'source')
+plot(xax, ex_sol)
+legend('vx', 'exact solution')
 hold off
