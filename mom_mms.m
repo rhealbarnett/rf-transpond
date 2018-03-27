59138f33b1df45c28e6af367425b88b2bfac0ee7
rhealbarnett <rhealbarnett@gmail.com>
2018-03-27 17:44:50 -0400

    testing order of convergence

--- mom_mms.m
+++ mom_mms.m
@@ -23,14 +23,15 @@ tax = linspace(tmin,nmax*dt,nmax);
 % spatial domain %
 %------
 xmin = 0;
-xmax = 1.0;
+xmax = 2.0*pi;
 
 % spatial step and axis
-dx = logspace(-5,-1,5);
+% dx = logspace(-2,0,10);
+dx = 1.;
 
 % coefficients for source term s = a + sin(x + ct)
-a = 1.0;
-c = 1.0;
+a = 0.0;
+c = -1.0;
 
 %%
 %-------------------------------------------------------------------------%
@@ -39,44 +40,56 @@ c = 1.0;
 %-------------------------------------------------------------------------%
 
 % diffusion term multiplier
-nu = 0.0;
+nu = 0.05;
 
-l_sec = zeros(1,length(dx));
-l_inf = zeros(1,length(dx));
+% number of dx iterations (ie times to perform dx=dx/2
+iter = 8;
+l_sec = zeros(1,iter);
+l_inf = zeros(1,iter);
 
-for kk=1:length(dx)
+ax = 0;
+
+for kk=1:iter
     
-    npts = round(1 + (xmax - xmin)/dx(kk));
+    npts = round(1 + (xmax - xmin)/dx);
     xax = linspace(xmin,xmax,npts);
     
     % initial conditions
     source = zeros(1,npts);
-    vx = zeros(1,npts);
-    vx_new = a + sin(xax) + source;
+    vx_new = a + sin(xax);
     
-    for ii=1
-
+    for ii=1:21
+        
         % exact solution
         ex_sol = a + sin(xax + c*dt*ii);
-
+        
         vx = vx_new;
 
         for jj=2:npts-1
 
             source(1,jj) = c*cos(c*ii*dt + xax(jj)) + (a + sin(xax(jj) + ii*dt*c))*cos(xax(jj) + c*ii*dt) + nu*sin(xax(jj) + c*ii*dt); 
 
-            vx_new(1,jj) = vx(1,jj) - ((dt)/(2.0*dx(kk)))*((vx(1,jj+1)^2)/2 - (vx(1,jj-1)^2)/2) +...
-                ((nu*dt)/(dx(kk)^2))*(vx(1,jj+1) - 2.0*vx(1,jj) + vx(1,jj-1)) + dt*source(1,jj);
+            vx_new(1,jj) = vx(1,jj) - (dt/(2.0*dx))*vx(1,jj)*(vx(1,jj+1) - vx(1,jj-1)) +...
+                (dt/(dx^2))*nu*(vx(1,jj+1) - 2.0*vx(1,jj) + vx(1,jj-1)) + dt*source(1,jj);
         end
 
         % boundary conditions
-        vx_new(1,end) = a + sin(xmax + c*dt*ii);
-        vx_new(1,1) = a + sin(c*dt*ii);
+        source(1,end) = c*cos(xmax + c*dt*ii) + (a + sin(xmax + ii*dt*c))*cos(xmax + c*ii*dt) + nu*sin(xmax + c*ii*dt);
+%         vx(1,end) = vx(1,end) - (dt/(2.0*dx))*vx(1,end)*(vx(1,2) - vx(1,end-1)) +...
+%                 (dt/(dx^2))*nu*(vx(1,2) - 2.0*vx(1,end) + vx(1,end-1)) + dt*source(1,end);
+%         vx(1,1) = vx_new(1,end);
         
-        l_inf(1,kk) = abs(max(vx_new - ex_sol));
-        l_sec(1,kk) = rms(vx_new - ex_sol);
+        vx(1,end) = a + sin(xmax + c*dt*ii) + source(1,end);
+        vx_new(1,1) = vx_new(1,end);
+
+        l_inf(1,kk) = max(abs(ex_sol - vx_new));
+        l_sec(1,kk) = rms(ex_sol - vx_new);
+       
 
     end
+    
+    ax(1,kk) = dx;
+    dx = dx/2.0;
     
 end
 
@@ -93,4 +106,15 @@ plot(xax, ex_sol)
 xlabel('Location')
 ylabel('Amplitude')
 legend('vx', 'exact solution')
+hold off
+
+figure(2)
+loglog(ax,l_sec, '-*r') 
+hold on
+loglog(ax,l_inf, '-ob')
+loglog(ax, ax.^2, '--k')
+% xlim([1e-2 1e0])
+xlabel('dx')
+ylabel('Error')
+legend('L$_2$', 'L$_{\inf}$', 'dx$^2$','Location','northwest')
 hold off
