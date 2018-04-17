1839240f0851f89d707e23177441536e44bc18f5
rhealbarnett <rhealbarnett@gmail.com>
2018-04-17 15:47:08 -0400

    added NaN check into momentum equation solve

--- cont_mom_test.m
+++ cont_mom_test.m
@@ -9,31 +9,14 @@
 c0 = 3.0e8;
 
 %--
-% temporal domain
-tmin = 0;
-% dt = 0.99*(dx);
-% dt = logspace(-5,-11,7);
-dt = 1.5e-13;
-tmax = 1.0e4*dt;
-% tmax = 2.5e-4;
-nmax = tmax/dt;
-% tax = linspace(tmin,tmax,nmax);
-tol = 1.0e-5;
-% dt = 0.99*dx;
-% nmax = 756;
-
-%--
 % spatial domain
 xmin = 0;
-xmax = 1;
-dx = logspace(-1,-5,5);
-% npts = (xmax-xmin)./dx;
-% xax = linspace(xmin,xmax,npts);
-% npts = length(xax);
+xmax = 2.0;
+dx = 1.125e-4;
+% npts = 128;
+npts = round((xmax - xmin)/(dx) + 1);
 % dx = (xmax - xmin)/(npts-1);
-% dx = 0.0013378;
-% xax = xmin:dx:xmax;
-% npts = length(xax);
+xax = linspace(xmin,xmax,npts);
 
 %--
 % constants
@@ -42,64 +25,91 @@ m = 1.67e-27;
 
 %--
 % parameters
-Te = 5.0;
+Te = 10.0;
 Ti = 5.0;
 T = Te + Ti;
-cs = sqrt((Te + Ti)*e/m);
-% cs = 10.0;
+% cs = sqrt((Te + Ti)*e/m);
+cs = 1.0e4;
+nu = 1.0;
 
-
+%--
+% temporal domain
+tmin = 0;
+% dt = 0.99*dx/c0;
+% dt = 8.0e-6;
+dt = 0.99*(dx^2)/(2.0*nu);
+tmax = 1.0e5*dt;
+nmax = tmax/dt;
+tol = 1.0e-14;
 
 %%
 
-%-------------------------------------------------------------------------%
-% Solve continuity equation with analytic velocity expression             %
-%-------------------------------------------------------------------------%
-
-%--
-% velocity
-vx = cs*xax;
-dvx = gradient(vx,xax);
-
-%--
-% plot velocity to compare against comsol output
-figure(1)
-plot(xax,vx)
-hold on
-plot(xax,dvx)
-legend('vx', 'dvx')
-xlabel('Position ($m$)')
-hold off
-
-%--
-% up wind differencing scheme
-
-n = zeros(1,npts);
-n(1,:) = 1.0e19;
-% n(1) = 1.0e19;
-
-for ii=1:nmax
-    n_old = n;
-    for jj=2:npts-1
-        n(1,jj) = n(1,jj) - vx(1,jj)*(dt/dx)*(n(1,jj) - n(1,jj-1)) - n(1,jj)*(dt)*dvx(1,jj);%/dx)*(vx(1,jj) - vx(1,jj-1));
-    end
-    n(1,1) = 1.0e19;
-    n(1,end) = n(1,end-1);
-%     if abs(rms(n_old)-rms(n))<tol
+% %-------------------------------------------------------------------------%
+% % Solve continuity equation with analytic velocity expression             %
+% %-------------------------------------------------------------------------%
+% 
+% %--
+% % velocity
+% vx = cs*xax;
+% dvx = gradient(vx,xax);
+% 
+% %--
+% % plot velocity to compare against comsol output
+% figure(1)
+% plot(xax,vx)
+% hold on
+% plot(xax,dvx)
+% legend('vx', 'dvx')
+% xlabel('Position ($m$)')
+% hold off
+% 
+% %--
+% % up wind differencing scheme
+% 
+% n_new = 1.0e19*ones(1,npts);
+% 
+% %--
+% % initialise matrix
+% coeff_mat = zeros(npts,npts);
+% coeff_mat(1,1) = 1.0;
+% coeff_mat(end,end) = 1.0;
+% 
+% for ii=1:nmax
+%     
+%     n = n_new;
+%     
+%     for jj=2:npts-1
+%         
+%         coeff_mat(jj,jj) = (dt/(2.0*dx))*(vx(1,jj-1) - vx(1,jj+1));
+%         coeff_mat(jj,jj-1) = (1.0/2.0) + (dt/(2.0*dx))*vx(1,jj);
+%         coeff_mat(jj,jj+1) = (1.0/2.0) - (dt/(2.0*dx))*vx(1,jj);
+%         
+%     end
+%     
+%     n_new = coeff_mat*n';
+%     
+%     n_new = n_new';
+%     
+%     n_new(1,1) = 1.0e19;
+%     n_new(1,end) = n_new(1,end-1);
+%     
+%     if rms(n - n_new)<=tol
+%         fprintf('tolerance reached, ii=%d\n',ii)
 %         break
-%         ii
 %     else
 %         continue
+%         
 %     end
-end
-
-
-%--
-% plot solution to compare with comsol solution
-figure(2)
-semilogy(xax,n)
-xlabel('Position ($m$)','Fontsize',16)
-ylabel('log$_{10}|$Density$|$','Fontsize',16)
+% 
+% end
+% 
+% 
+% %--
+% % plot solution to compare with comsol solution
+% figure(2)
+% semilogy(xax,n)
+% xlabel('Position ($m$)','Fontsize',16)
+% ylabel('log$_{10}|$Density$|$','Fontsize',16)
 
 %%
 %-------------------------------------------------------------------------%
@@ -107,102 +117,85 @@ ylabel('log$_{10}|$Density$|$','Fontsize',16)
 %-------------------------------------------------------------------------%
 
 clear vx dvx n
-% 
-% %--
-% % density
-% Nmax = 19;
-% Nmin = 16;
-% m = (Nmax - Nmin) ./ (xmax - xmin);
-% n = 10.^(-m*xax + Nmax);
-% n = 1.0e19*ones(1,npts);
-% % n = 0.5e19*(xax)+1.05e19;
-% dnx = gradient(n,xax);
-% 
-% %--
-% % plot density to compare against comsol output
-% figure(3)
-% plot(xax,n)
-% hold on
-% plot(xax,dnx)
-% legend('n', 'dnx')
-% xlabel('Position ($m$)')
-% hold off
 
 %--
-% up wind differencing scheme
-% CONSERVATIVE FORM ie (all partials) dv/dt + d(v^2/2)/dx = ...
+% density
+% Nmax = 1.0e19;
+% n = nmax*ones(1,npts);
+n = normpdf(xax,1,0.05);
+n = n/max(n);
+n = 1.0e3*n;
+n = n + 1;
+n = n*1.0e16;
+dnx = gradient(n,xax);
 
-% vx = (cs)*ones(1,npts);
-% vx = zeros(1,npts);
-% vx(1,1) = 0;
-% vx(1,end) = cs;
-% vx = sin(xax);
-% % a = find(xax<=0.5);
-% % vx(a(end):end) = 1.0;
-% vx_new = zeros(1,npts);
-% source = zeros(1,npts);
-nu = 1.0e-5;
+%--
+% plot density to compare against comsol output
+figure(3)
+plot(xax,n)
+hold on
+plot(xax,dnx)
+legend('n', 'dnx')
+xlabel('Position ($m$)')
+hold off
 
-% figure(4)
-% plot(xax,vx,'--k')
-% hold on
+%--
+% initialise velocity coefficient matrix and rhs
+vxA = zeros(npts,npts);
+vx_source = zeros(npts,1);
 
+%--
+% initial value for velocity
+vx_new = (cs/4.0)*xax + cs/2.0;
+% vx_new = cs*xax;
 
-for kk=length(dx)
-    kk
-    for ii=1:nmax
-        npts = round((xmax-xmin)/dx(kk));
-        xax = linspace(xmin,xmax,npts);
-        vx = sin(xax);
-        source = zeros(1,npts);
-        l_inf = zeros(1,npts);
-        l_sec = zeros(1,npts);
-%         tax = linspace(tmin,tmax,round(nmax(kk)));
-        for jj=2:npts-2
-            vx(1,jj) = (1./2.)*(vx(1,jj-1) + vx(1,jj+1)) - vx(1,jj)*(dt/(2.0*dx(kk)))*((vx(1,jj+1)) - (vx(1,jj-1))) + ...
-                (dt*nu/(dx(kk)^2))*(vx(1,jj) - 2.0*vx(1,jj+1) + vx(1,jj+2));
-%             source(1,jj) = - cos(ii*dt(kk) - xax(jj)) - (1.0/2.0)*sin(2.0*(ii*dt(kk) - xax(jj))); 
-            source(1,jj) = - cos(ii*dt - jj*dx(kk)) + sin(ii*dt - jj*dx(kk))*cos(jj*dx(kk) - ii*dt) + nu*sin(jj*dx(kk) - ii*dt);
-%             source(1,jj) = - exp(xax(jj) - ii*dt(kk)) + exp(2.0*(xax(jj) - ii*dt(kk)));
-        end
-        vx(1,1) = sin(-ii*dt);
-        vx(1,end) = sin(npts*dx(kk) - ii*dt);
-        source(1,1) = sin(-ii*dt);
-        source(1,end) = sin(npts*dx(kk) - ii*dt);
-        l_inf(1,ii) = abs(max(vx - source));
-        l_sec(1,ii) = rms(vx - source);
+for ii=1:nmax
+    
+    vx = vx_new;
+
+    for jj=2:npts-1
         
-    %     vx_new(1,1) = vx(1,1);
-    %     vx(1,end) = vx(1,end-1);
-    %     vx = vx_new;
-    %     if (ii==100) || (ii==300) || (ii==600) || (ii==756)
-    %         figure(5)
-    %         plot(xax,vx_new)
-    %         hold on
-    %     if abs(rms(vx_old)-rms(vx))<1.0e-9
-    %         break
-    %     elseif isnan(vx(end-1))
-    %         break
-    %     else
-    %         continue
-    %     end
-    %     end
+        vxA(jj,jj) = (2.0*nu*dt)/(dx^2) - 1.0;
+        vxA(jj,jj-1) = -(vx(1,jj-1)*dt)/(4.0*dx) - (nu*dt)/(dx^2);
+        vxA(jj,jj+1) = (vx(1,jj+1)*dt)/(4.0*dx) - (nu*dt)/(dx^2);
+        
+        vx_source(jj,1) = 0.0;%-((Te + Ti)*e)/(m*n(1,jj)*2.0*dx)*(n(1,jj+1) - n(1,jj-1)); 
+
+    end
+    
+    vxA = sparse(vxA);
+    
+    vx_new = vx_source - vxA*vx';
+  
+    vx_new(1,1) = cs/2;
+    vx_new(end,1) = cs;
+   
+    vx_new = vx_new';
+    
+    nan_check = isnan(vx_new);
+    
+    if sum(nan_check) ~= 0
+        fprintf('unstable, ii=%d\n',ii)
+        break
+    else
+        continue
+    end
+    
+    if (rms(vx - vx_new) <= tol)
+        fprintf('tolerance reached, ii=%d\n',ii)
+        break
+    else
+        continue
+        
     end
 end
-% figure(5)
-% legend('t=0s', 't=0.13s', 't=0.4s', 't=0.8s', 't=1.0s','Location','northwest')
-% 
-% hold off
-    
+
+%%
 %--
-% % plot solution to compare with comsol solution
-figure(5)
-plot(xax,vx)
-xlabel('Position ($m$)','Fontsize',16)
-ylabel('Velocity ($ms^{-1}$)','Fontsize',16)
 
 figure(6)
-plot(xax,source) 
+plot(xax,vx_source) 
 hold on
-plot(xax,vx)
+plot(xax,vx_new)
 legend('source','vx')
+hold off
