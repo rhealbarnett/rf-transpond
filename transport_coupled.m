d5a2e26cb630afb4dfe1fde9a6d929507876eb7b
rhealbarnett <rhealbarnett@gmail.com>
2018-04-23 15:51:11 -0400

    attempt to couple as mom solve is stable

--- transport_coupled.m
+++ transport_coupled.m
@@ -8,15 +8,6 @@
 %----------------------------------------%
 
 %------
-% spatial domain %
-%------
-xmin = 0.0;
-xmax = 1.0;
-npts = 128;
-dx = (xmax - xmin)/(npts - 1);
-xax = linspace(xmin,xmax,npts);
-
-%------
 % constants %
 %------
 e = 1.6022e-19;
@@ -33,14 +24,22 @@ cs = sqrt((Te + Ti)*e/m);
 nu = 1.0;
 
 %------
+% spatial domain %
+%------
+xmin = 0.0;
+xmax = 10/cs;
+npts = 64;
+dx = (xmax - xmin)/(npts - 1);
+xax = linspace(xmin,xmax,npts);
+
+%------
 % temporal domain %
 %------
 tmin = 0;
 
 % calculate dt based on vacuum speed of light for now
-dt = 0.99*dx/c0;
-nmax = 1.0e5;
-tol = 1.0e-14;
+dt = 8.0e-10;
+nmax = 1.0e7;
 
 %%
 
@@ -49,9 +48,14 @@ tol = 1.0e-14;
 % Initialise coefficient matrices                                         %
 %-------------------------------------------------------------------------%
 
-n_new = 1.0e19*ones(1,npts);
+n_new = normpdf(xax,xmax/2,xmax/30);
+n_new = n_new/max(n_new);
+n_new = 1.0e3*n_new;
+n_new = n_new + 1;
+n_new = n_new*1.0e16;
+dnx = gradient(n_new,xax);
 
-vx_new = cs*xax;
+vx_new = zeros(1,npts);
 nA = zeros(npts,npts);
 vxA = zeros(npts,npts);
 vx_source = zeros(npts,1);
@@ -63,11 +67,23 @@ vxA(end,end) = 1.0;
 
 %%
 
+figure(1)
+semilogy(xax,n_new,'DisplayName','time = 0s')
+hold on
+
 %-------------------------------------------------------------------------%
 % Solve                                                                   %
 %-------------------------------------------------------------------------%
 
 for ii=1:nmax
+
+%     if (0.99*(dx^2)/(2.0*nu))<(0.99*dx/max(vx_new))
+%         dt = 0.99*(dx^2)/(2.0*nu);
+%         continue
+%     elseif (0.99*(dx^2)/(2.0*nu))>(0.99*dx/max(vx_new))
+%         dt = 0.99*dx/max(vx_new);
+%         continue
+%     end
     
     vx = vx_new;
     n = n_new;
@@ -82,7 +98,7 @@ for ii=1:nmax
         vxA(jj,jj-1) = -(vx(1,jj-1)*dt)/(4.0*dx) - (nu*dt)/(dx^2);
         vxA(jj,jj+1) = (vx(1,jj+1)*dt)/(4.0*dx) - (nu*dt)/(dx^2);
         
-        vx_source(jj,1) = 0;%-((Te + Ti)*e)/(m*n(1,jj)*2.0*dx)*(n(1,jj+1) - n(1,jj-1)); 
+        vx_source(jj,1) = -((Te + Ti)*e)/(m*n(1,jj)*2.0*dx)*(n(1,jj+1) - n(1,jj-1)); 
 
     end
     
@@ -96,26 +112,42 @@ for ii=1:nmax
     vx_new = vx_new';
     
     vx_new(1,1) = 0;
-    vx_new(1,npts) = cs;
-    n_new(1,1) = 1.0e19;
+    vx_new(1,npts) = 0;
+    n_new(1,1) = n_new(1,2);
     n_new(1,end) = n_new(1,end-1);
     
-    if (rms(n - n_new)<=tol) || (rms(vx - vx_new)<=tol)
-        fprintf('tolerance reached, ii=%d\n',ii)
+    nan_check = isnan(vx_new);
+    
+    if sum(nan_check) ~= 0
+        fprintf('unstable, ii=%d\n',ii)
+        break
+    elseif dt*max(vx_new)/dx >= 1.0 || dt*2*nu/dx^2 >= 1.0
+        fprintf('CFL condition violated')
         break
     else
         continue
-        
-    end
+    end 
+    
+    
+%     figure(1)
+%     plot(xax,n_new,'DisplayName',['time = ' num2str(ii*dt) ' s'])
+%     hold on
+    
 end
 
-figure(1)
-semilogy(xax,n)
+%%
+
+semilogy(xax,n_new,'--r','DisplayName',['time = ' num2str(nmax*dt) ' s'])
+legend('show')
+hold off
+
+figure(2)
+semilogy(xax,n_new)
 xlabel('Position ($m$)','Fontsize',16)
 ylabel('log$_{10}|$Density$|$','Fontsize',16)
 
-figure(2)
-plot(xax,vx)
+figure(3)
+plot(xax,vx_new)
 xlabel('Position ($m$)','Fontsize',16)
 ylabel('Velocity ($ms^{-1}$)','Fontsize',16)
 
