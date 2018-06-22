f7144851dc7f46b787cb9c3a11d05905428a3979
rhealbarnett <rhealbarnett@gmail.com>
2018-06-22 13:24:18 -0400

    edits on the neutral density profile for testing

--- transport_large.m
+++ transport_large.m
@@ -31,7 +31,7 @@ xmax = 1.0;
 %-- include two additional gridpoints for the density ghost points
 %-- velocity grid will then be defined as having npts-1 (xax(1:npts-1))
 %-- density solution space will be defined as having npts-2 (xax(2:npts-1))
-npts = 128;
+npts = 512;
 dx = (xmax - xmin)/(npts - 1);
 nxax = linspace(xmin-0.5*dx,xmax+0.5*dx,npts);
 vxax = linspace(xmin,xmax,npts-1);
@@ -60,8 +60,8 @@ dnx = gradient(n_new,nxax);
 %-- density source
 rate_coeff = 10e-14;
 decay_index = round(npts/2.5);
-neut_max = 18.5;
-neut_min = 16;
+neut_max = 17;
+neut_min = 14;
 decay_length = 0.4;
 decay_gradient = (neut_min - neut_max)/decay_length;
 % n_neut = (rate_max - rate_min)*exp(-90.0*nxax(1,1:end/2)) + rate_min;
@@ -70,11 +70,11 @@ n_neut(1:decay_index + 1) = 10.^(decay_gradient*nxax(1:decay_index + 1) + neut_m
 n_neut(end-decay_index:end) = fliplr(n_neut(1:decay_index + 1));
 % n_neut = [n_neut,fliplr(n_neut)];
 n_neut = n_neut';
-n_neut(decay_index+1:end-decay_index) = 0.0;%10^neut_min;
+n_neut(decay_index+1:end-decay_index) = 10^neut_min;
 n_source = zeros(npts,1);
 
 for ii=1:npts
-    n_source(ii,1) = n_neut(ii,1)*n_new(1,ii)*rate_coeff;
+    n_source(ii,1) = n_neut(ii,1)*n_neut(ii,1)*rate_coeff;
 end
 
 %-- initial velocity
@@ -103,7 +103,7 @@ vxA(1,1) = 1.0;
 vxA(end,end) = 1.0;
 
 %-- set dt based on CFL conditions, check during loop if violated
-tmax = 1.0e-3;
+tmax = 1.0e-4;
 if (0.99*(dx^2)/(2.0*nu))<(0.99*dx/max(abs(vx_new)))
     dt = 0.99*(dx^2)/(2.0*nu);
 elseif (0.99*(dx^2)/(2.0*nu))>(0.99*dx/max(abs(vx_new)))
