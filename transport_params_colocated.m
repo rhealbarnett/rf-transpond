5c90d76b6bb737ec539956b84f8149312aef8f6d
rhealbarnett <rhealbarnett@gmail.com>
2018-08-23 16:12:26 -0400

    various edits to test the collocated parameters with the transport_1d code

--- transport_params_colocated.m
+++ transport_params_colocated.m
@@ -22,6 +22,7 @@ T = Te + Ti;
 cs = sqrt((Te + Ti)*e/m);
 % cs = 10;
 nu = 1000.0;
+% nu = 0;
 
 %------
 % spatial domain %
@@ -32,10 +33,10 @@ xmax = 1.0;
 % include two additional gridpoints for the density ghost points
 % velocity grid will then be defined as having npts-1 (xax(1:npts-1)) --
 % density solution space will be defined as having npts-2 (xax(2:npts-1))
-npts = 32;
+npts = 512;
 dx = (xmax - xmin)/(npts - 1);
-nxax = linspace(xmin,xmax,npts);
-vxax = linspace(xmin,xmax,npts);
+nxax = linspace(xmin,xmax,npts-1);
+vxax = linspace(xmin,xmax,npts-1);
 
 %------
 % temporal domain %
@@ -52,59 +53,53 @@ tmin = 0;
 %-- initial density profile
 Nmax = 16;
 Nmin = 15;
-n_new = normpdf(nxax,(xmax)/2,(xmax)/30);
-n_new = (10^Nmax)*n_new/max(n_new);
-% n_new = (Nmax)*ones(1,npts);
+% n_new = normpdf(nxax,(xmax)/2,(xmax)/30);
+% n_new = (10^Nmax)*n_new/max(n_new);
+n_new = (10^Nmax)*ones(1,npts-1);
 
 %-- density source
 rate_coeff = 10e-14;
-decay_index = round((npts)/2.5);
+decay_index = round((npts-1)/2.5);
 cosax = linspace(0,pi,decay_index+1);
-neut_max = 18.5;
+neut_max = 16.5;
 neut_min = 14;
 decay_length = 0.4;
 decay_gradient = (neut_min - neut_max)/decay_length;
 % n_neut = (rate_max - rate_min)*exp(-90.0*nxax(1,1:end/2)) + rate_min;
-n_neut = zeros(1,npts);
+n_neut = zeros(1,npts-1);
 % n_neut(1:decay_index + 1) = 10.^(decay_gradient*nxax(1:decay_index + 1) + neut_max);
-n_neut(1:decay_index+1) = 10^neut_max*(cos(cosax)+1.01)/2;
-% n_neut(end-decay_index:end) = fliplr(n_neut(1:decay_index + 1));
+n_neut(1:decay_index+1) = (10^neut_max)*(cos(cosax)+1.01)/2;
+n_neut(end-decay_index:end) = fliplr(n_neut(1:decay_index + 1));
 % % n_neut = [n_neut,fliplr(n_neut)];
 % n_neut = n_neut';
 % n_neut(1:end-decay_index) = (n_neut(decay_index)/2);
 % n_neut = fliplr(n_neut);
-n_source = zeros((npts),1);
+n_source = zeros((npts-1),1);
 
-% for ii=1:npts-2
-%     n_source(ii,1) = n_neut(1,ii)*n_neut(1,ii)*rate_coeff;
-% end
+for ii=1:npts-1
+    n_source(ii,1) = n_neut(1,ii)*n_neut(1,ii)*rate_coeff;
+end
 
 %-- initial velocity
-% vx_ax = linspace(0,1,npts);
-% vx_new = (cs/2)*vx_ax + cs/2;
-% vx_new(1,1) = cs/2;
-vx_new = zeros(1,npts);
-vx_new(1:npts/2) = -cs;
-vx_new(npts/2:npts) = cs;
+vx_ax = linspace(-1,1,npts-1);
+vx_new = cs*vx_ax;
+% vx_new(1,1) = -cs/2;
+% vx_new = zeros(1,npts-1);
+% vx_new(1:(npts-1)/2) = -cs;
+% vx_new((npts-1)/2:npts-1) = cs;
 
 
 %-- initialise coefficient matrices for density, velocity, and momentum equation 
 %-- rhs 'source' term
-nA = zeros(npts,npts);
-vxA = zeros(npts,npts);
-vx_source = zeros(npts,1);
-
-%-- fill boundary conditions in coefficient matrix
-%-- Dirichlet conditions on velocity 
-vxA(1,1) = 1.0;
-vxA(1,end) = 1.0;
-nA(1,1) = 1.0;
-nA(1,end) = 1.0;
+nA = zeros(npts-1,npts-1);
+vxA = zeros(npts-1,npts-1);
+vx_source = zeros(npts-1,1);
 
 %-- set dt based on CFL conditions, check during loop if violated
-tmax = 1.0e-3;
+tmax = 1.0e-4;
 cfl_fact = 0.8;
-if (cfl_fact*(dx^2)/(2.0*nu))<(cfl_fact*dx/max(abs(vx_new)))
+
+if ((cfl_fact*(dx^2)/(2.0*nu))<(cfl_fact*dx/max(abs(vx_new))))
     dt = cfl_fact*(dx^2)/(2.0*nu);
 elseif (cfl_fact*(dx^2)/(2.0*nu))>(cfl_fact*dx/max(abs(vx_new)))
     dt = cfl_fact*dx/max(abs(vx_new));
@@ -112,7 +107,7 @@ else
     dt = cfl_fact*dx/cs;
 end
 
-dt = cfl_fact*dx/cs;
+mult = dt/dx;
 nmax = round(tmax/dt);
 tax = linspace(tmin,tmax,nmax);
 
