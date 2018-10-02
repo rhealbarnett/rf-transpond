e6df101552a52d5778117ac399ff78a2f8e3b501
rhealbarnett <rhealbarnett@gmail.com>
2018-10-02 12:25:55 -0400

    steady state mms worked; moved to time dependent but not working yet

--- mms_implicit.m
+++ mms_implicit.m
@@ -15,6 +15,7 @@
 % constants %
 %------
 constants;
+e = const.e;
 % m = mp;
 m = 0.01;
 
@@ -36,14 +37,18 @@ xmax = 0.1;
 % include two additional gridpoints for the density ghost points
 % velocity grid will then be defined as having npts-1 (xax(1:npts-1)) --
 % density solution space will be defined as having npts-2 (xax(2:npts-1))
-npts = 11;
+npts = 1281;
 xax = linspace(xmin,xmax,npts);
+dx = (xmax - xmin)/(npts - 1);
 
 %------
 % temporal domain %
 %------
 tmin = 0;
-nmax = 1.0e7;
+tmax = 8.0e-4;
+dt = 8.0e-6;
+% nmax = 1.0e7;
+nmax = round(tmax/dt);
 
 %%
 
@@ -55,7 +60,7 @@ nmax = 1.0e7;
 
 u0 = 1.0;
 n0 = 2.0;
-om = 1.0e3;
+om = 0.0;
 epsilon = 0.001;
 
 vx_new = u0*(sin(xax.^2) + epsilon);
@@ -69,6 +74,9 @@ vx_pos = sparse(npts,npts);
 vx_neg = sparse(npts,npts);
 vx_diff = sparse(npts,npts);
 vx_I = eye(npts,npts);
+source_dt = zeros(1,npts);
+source_dx = zeros(1,npts);
+source_dxx = zeros(1,npts);
 
 
 %%
@@ -79,80 +87,98 @@ vx_I = eye(npts,npts);
 
 tol = 1.0e-14;
 iter = 8;
+r = 2.0;
+count = 1;
 l_inf = zeros(1,iter);
 l_two = zeros(1,iter);
 dx_arr = zeros(1,iter);
-
+dt_arr = zeros(1,iter);
 
 for kk=1:iter
 
     fprintf('counter=%d\n',kk)
 
-    dx = (xmax - xmin)/(npts - 1);
-    dx_arr(1,kk) = dx;
+%     dx = (xmax - xmin)/(npts - 1);
+%     dx_arr(1,kk) = dx;
+    dt_arr(1,kk) = dt;
     xax = linspace(xmin,xmax,npts);
-    mult = 1.0/dx;
-
-    fprintf('dx=%d\n',dx)
+    fprintf('dt=%d\n',dt)
 
      % initial conditions
-    vx_new = u0*sin(xax.^2 + epsilon);
+    vx_new = u0*(sin(xax.^2) + epsilon);
     n_new = n0*cos(xax.^2);
     rhs = gradient(n_new);
     vx_pos = sparse(npts,npts);
     vx_neg = sparse(npts,npts);
     vx_diff = sparse(npts,npts);
     vx_I = eye(npts,npts);
+    source_dt = zeros(1,npts);
+    source_dx = zeros(1,npts);
+    source_dxx = zeros(1,npts);
      
     for ii=1:nmax
 
-        ex_sol = u0*(sin(xax.^2) + epsilon);
+        ex_sol = u0*(sin(xax.^2 + om*dt*ii) + epsilon);
 
         vx = vx_new;
-
+       
         for jj=2:npts-1
             if vx(1,jj)>0
-                vx_pos(jj,jj) = - mult*vx(1,jj);
-                vx_pos(jj,jj-1) = mult*vx(1,jj);
+                vx_pos(jj,jj) = - vx(1,jj)/dx;
+                vx_pos(jj,jj-1) = vx(1,jj)/dx;
             elseif vx(1,jj)<0
-                vx_neg(jj,jj) = mult*vx(1,jj);
-                vx_neg(jj,jj+1) = -mult*vx(1,jj);
+                vx_neg(jj,jj) = vx(1,jj)/dx;
+                vx_neg(jj,jj+1) = -vx(1,jj)/dx;
             end
-            vx_diff(jj,jj) = - mult*((2.0*nu)/dx);
-            vx_diff(jj,jj-1) = mult*(nu/dx);
-            vx_diff(jj,jj+1) = mult*(nu/dx);
+            vx_diff(jj,jj) = - (2.0*nu)/(dx^2);
+            vx_diff(jj,jj-1) = (nu/(dx^2));
+            vx_diff(jj,jj+1) = (nu/(dx^2));
         end
 
-        vxA = vx_I + vx_pos + vx_neg + vx_diff;
-        vxA(1,1) = 1.0; vxA(end,end) = 1.0;
-
-        source_dx = 2.0*u0*xax.*cos(xax.^2);
-        source_dxx = 2.0*u0*(cos(xax.^2) - 2.0*xax.^2.*sin(xax.^2));
+        vxA = vx_pos + vx_neg + vx_diff;
+        Avx = vx_I - dt*vxA;
+        Avx(1,1) = 1.0; Avx(end,end) = 1.0;
+        vx(1,1) = u0*(sin(xmin^2 + om*dt*ii) + epsilon);
+        vx(1,end) = u0*(sin(xmax^2 + om*dt*ii) + epsilon);
+    
+        source_dt = u0*om*cos(xax.^2 + om*dt*ii);
+        source_dx = 2.0*u0*xax.*cos(xax.^2 + om*dt*ii)*u0.*(sin(xax.^2 + om*dt*ii) + epsilon);
+        source_dxx = 2.0*u0*(cos(xax.^2 + om*dt*ii) - 2.0*xax.^2.*sin(xax.^2 + om*dt*ii));
         source_nx = -2.0*n0*xax.*sin(xax.^2);
 
-        source = vx.*source_dx + nu*source_dxx - source_nx;
+        source = source_dt + source_dx - nu*source_dxx;
+%         source(1,1) = u0*(sin(xmin.^2 + om*dt*ii) + epsilon);
+%         source(1,end) = u0*(sin(xmax.^2 + om*dt*ii) + epsilon);
 
-        vx_new = vxA\(source' + rhs');
+        vx_new = Avx\(vx');% + source');
 
         vx_new = vx_new';
 
         l_inf(1,kk) = norm(ex_sol - vx_new, Inf);
         l_two(1,kk) = rms(ex_sol - vx_new);
 
-        if rms(vx_new - vx)<=tol
-            fprintf('tolerance reached, ii=%d\n',ii)
-            break
-        else
-            continue
-        end
+%         if rms(vx_new - vx)<=tol
+%             fprintf('tolerance reached, ii=%d\n',ii)
+%             break
+%         elseif ii==count*100
+%             fprintf('ii=%d\n',ii)
+%             fprintf('rms=%d\n',rms(vx_new - vx))
+%             count = count + 1;
+%             continue
+%         end
+        
+        
     end
 
-    npts = 2.0*npts-1;
+%     npts = r*npts-1;
+    dt = dt/r;
      
 end
 
 ratio_inf = l_inf(1:iter-1)./l_inf(2:iter);
 ratio_two = l_two(1:iter-1)./l_two(2:iter);
+oo_inf = log(ratio_inf)/log(2);
+oo_two = log(ratio_two)/log(2);
 
 %%
 %------ 
@@ -163,8 +189,8 @@ figure(1)
 plot(xax, vx, '*k')
 hold on
 plot(xax, ex_sol)
-xlabel('Location')
-ylabel('Amplitude')
+% xlabel('Location')
+% ylabel('Amplitude')
 legend('vx', 'exact solution')
 hold off
 
@@ -172,11 +198,11 @@ figure(2)
 loglog(dx_arr,l_two, '-*r') 
 hold on
 loglog(dx_arr,l_inf, '-ob')
-loglog(dx_arr, dx_arr.^2, '--k')
+loglog(dx_arr, dx_arr, '--k')
 % xlim([5e-4 1e-1])
-xlabel('dx','Fontsize',16)
-ylabel('Error','Fontsize',16)
-legend({'L$_2$', 'L$_{\inf}$', 'dx$^2$'},'Fontsize',16,'Location','northwest')
+% xlabel('dx')
+% ylabel('Error')
+legend({'L_2', 'L_{\infty}', 'dx'},'Location','northwest')
 hold off
 
 
