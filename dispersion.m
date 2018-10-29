db3d004f0386f6961b341f3a389374ecae7f31da
rhealbarnett <rhealbarnett@gmail.com>
2018-10-29 18:00:08 -0400

    edits to plot labelling

--- dispersion.m
+++ dispersion.m
@@ -14,8 +14,8 @@ kperp_arr = complex(kperp_arr);
 kp1_arr = zeros(npts, 1);
 kp2_arr = zeros(npts, 1);
 
-kpara = kz;
-npara = c0*kpara/om;
+% kpara = kz;
+npara = c0*k_para/om;
 
 a1 = s_arr;
 b1 = r_arr.*l_arr + p_arr.*s_arr - npara^2*(p_arr + s_arr);
@@ -41,7 +41,7 @@ kperp_arr(:,4) = kp22;
 %%
 %--
 % plot kperps's
-imme = find(imag(me)==0);
+imme = find(imag(m)==0);
 imme = imme(end)+1;
 
 %--
@@ -54,69 +54,69 @@ yp22 = sign(kp22).*log10(abs(kp22));
 %%
 
 figure(9)
-plot(log10(N0),real(yp11),'.k')
+plot(log10(n_new),real(yp11),'.k')
 
 hold on
 
-plot(log10(N0),imag(yp11),'.r')
-plot(log10(N0),real(yp12),'dk','MarkerSize',3)
-plot(log10(N0),imag(yp12),'dr','MarkerSize',3)
-legend('Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]', 'Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]')
-xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-vline(log10(N0(imme)),'--k')
-ylabel('log$_{10}|$k$_{\perp1}|$','Fontsize',16)
+plot(log10(n_new),imag(yp11),'.r')
+plot(log10(n_new),real(yp12),'dk','MarkerSize',3)
+plot(log10(n_new),imag(yp12),'dr','MarkerSize',3)
+legend('Re[k_{\perp1}]', 'Im[k_{\perp1}]', 'Re[k_{\perp1}]', 'Im[k_{\perp1}]')
+xlabel('log_{10}|n|','Fontsize',16)
+% vline(log10(N0(imme)),'--k')
+ylabel('log_{10}|k_{\perp1}|','Fontsize',16)
 
 hold off
 
 figure(10)
-plot(log10(N0),real(yp21),'.k')
+plot(log10(n_new),real(yp21),'.k')
 
 hold on
 
-plot(log10(N0),imag(yp21),'.r')
-plot(log10(N0),real(yp22),'dk','MarkerSize',3)
-plot(log10(N0),imag(yp22),'dr','MarkerSize',3)
-legend('Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]', 'Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]')
-xlabel('log$_{10}|$N$_0|$','Fontsize',16)
-vline(log10(N0(imme)),'--k')
-ylabel('log$_{10}|$k$_{\perp2}|$','Fontsize',16)
+plot(log10(n_new),imag(yp21),'.r')
+plot(log10(n_new),real(yp22),'dk','MarkerSize',3)
+plot(log10(n_new),imag(yp22),'dr','MarkerSize',3)
+legend('Re[k_{\perp2}]', 'Im[k_{\perp2}]', 'Re[k_{\perp2}]', 'Im[k_{\perp2}]')
+xlabel('log_{10}|n|','Fontsize',16)
+% vline(log10(N0(imme)),'--k') 
+ylabel('log_{10}|k_{\perp2}|','Fontsize',16)
 
 hold off
 
 %%
 
-%%
-
-figure(9)
-plot(xax(npts-imme+1:imme-1),real(kp11(npts-imme+1:imme-1)),'.k')
-
-hold on
-
-plot(xax(npts-imme+1:imme-1),imag(kp11(npts-imme+1:imme-1)),'.r')
-plot(xax(npts-imme+1:imme-1),real(kp12(npts-imme+1:imme-1)),'dk','MarkerSize',3)
-plot(xax(npts-imme+1:imme-1),imag(kp12(npts-imme+1:imme-1)),'dr','MarkerSize',3)
-legend('Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]', 'Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]')
-xlabel('Position','Fontsize',16)
-% vline(xax(imme),'--k')
-ylabel('k$_{\perp1}$','Fontsize',16)
-
-hold off
-
-figure(10)
-plot(xax(npts-imme+1:imme-1),real(kp21(npts-imme+1:imme-1)),'.k')
-
-hold on
-
-plot(xax(npts-imme+1:imme-1),imag(kp21(npts-imme+1:imme-1)),'.r')
-plot(xax(npts-imme+1:imme-1),real(kp22(npts-imme+1:imme-1)),'dk','MarkerSize',3)
-plot(xax(npts-imme+1:imme-1),imag(kp22(npts-imme+1:imme-1)),'dr','MarkerSize',3)
-legend('Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]', 'Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]')
-xlabel('Position','Fontsize',16)
-% vline(xax(imme),'--k')
-ylabel('k$_{\perp2}$','Fontsize',16)
-
-hold off
-
-
-
-
+% %%
+% 
+% figure(9)
+% plot(xax(npts-imme+1:imme-1),real(kp11(npts-imme+1:imme-1)),'.k')
+% 
+% hold on
+% 
+% plot(xax(npts-imme+1:imme-1),imag(kp11(npts-imme+1:imme-1)),'.r')
+% plot(xax(npts-imme+1:imme-1),real(kp12(npts-imme+1:imme-1)),'dk','MarkerSize',3)
+% plot(xax(npts-imme+1:imme-1),imag(kp12(npts-imme+1:imme-1)),'dr','MarkerSize',3)
+% legend('Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]', 'Re[k$_{\perp1}$]', 'Im[k$_{\perp1}$]')
+% xlabel('Position','Fontsize',16)
+% % vline(xax(imme),'--k')
+% ylabel('k$_{\perp1}$','Fontsize',16)
+% 
+% hold off
+% 
+% figure(10)
+% plot(xax(npts-imme+1:imme-1),real(kp21(npts-imme+1:imme-1)),'.k')
+% 
+% hold on
+% 
+% plot(xax(npts-imme+1:imme-1),imag(kp21(npts-imme+1:imme-1)),'.r')
+% plot(xax(npts-imme+1:imme-1),real(kp22(npts-imme+1:imme-1)),'dk','MarkerSize',3)
+% plot(xax(npts-imme+1:imme-1),imag(kp22(npts-imme+1:imme-1)),'dr','MarkerSize',3)
+% legend('Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]', 'Re[k$_{\perp2}$]', 'Im[k$_{\perp2}$]')
+% xlabel('Position','Fontsize',16)
+% % vline(xax(imme),'--k')
+% ylabel('k$_{\perp2}$','Fontsize',16)
+% 
+% hold off
+% 
+% 
+% 
+% 
