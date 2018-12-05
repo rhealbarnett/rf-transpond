b56177764c5b0d03bfc97a7f6537971aebed1c3f
rhealbarnett <rhealbarnett@gmail.com>
2018-12-05 18:54:17 -0500

    changed to a function so it's easier to call

--- constants.m
+++ constants.m
@@ -9,12 +9,12 @@
 % mu0 = 4.0*pi*1.0e-7;
 % eps0 = 8.85e-12;
 % c0 = 1.0/sqrt(eps0*mu0);
-% function const = constants()
-const.e = 1.6022e-19;
-const.mp = 1.67e-27;
-const.me = 9.11e-31;
-const.kb = 1.38e-23;
-const.mu0 = 4.0*pi*1.0e-7;
-const.eps0 = 8.85e-12;
-const.c0 = 1.0/sqrt(const.eps0*const.mu0);
-% end
+function const = constants()
+    const.e = 1.6022e-19;
+    const.mp = 1.67e-27;
+    const.me = 9.11e-31;
+    const.kb = 1.38e-23;
+    const.mu0 = 4.0*pi*1.0e-7;
+    const.eps0 = 8.85e-12;
+    const.c0 = 1.0/sqrt(const.eps0*const.mu0);
+end
