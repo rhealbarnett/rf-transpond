34796a20ff879291ebc482cbd26a903158ed7685
rhealbarnett <rhealbarnett@gmail.com>
2019-01-25 16:34:52 -0500

    edited to include manufactured solution with sharp feature near boundary

--- transport_mms.m
+++ transport_mms.m
@@ -63,9 +63,38 @@ if variable
 
     % calculate the x values from xi
     x = xmax*(s.^(1/ro));
+%     x = (10.0/xmax)*s.^2;
+    
+    figure(1);
+    plot(s,x,'LineWidth',2);
+    hold on
+    
+    for ii=1:round(n/50):n
+        
+        figure(1)
+        hold on
+        plot([s(ii),s(ii)],[0.0, x(ii)]);
+        plot([0.0,s(ii)],[x(ii), x(ii)]);
+    
+    end
+    
+    xlabel('\xi','Fontsize',16) 
+    ylabel('x','Fontsize',16)
+    set(gca,'Box','on'); 
+    set(gca,'Fontsize',16, 'LineWidth',2)
+    plot([smin,smax],[0,L],'r')
+    % text(0.05,L-0.1,['A=',num2str(A),...
+    % ' x=',num2str(xc)],'Fontsize',18)
+
+    hold off;
 
     vxax = x;
     vdx = (vxax(2:end) - vxax(1:end-1));
+    vdx = fliplr(vdx);
+    vxax(1) = xmin;
+    for ii=2:(npts/2)-1
+        vxax(1,ii) = vxax(1,ii-1) + vdx(1,ii-1);
+    end
 
     npts = length(vxax) + 1;
     nxax = zeros(1,npts);
@@ -85,10 +114,12 @@ end
 
 % equib = load('equib2.mat');
 
+decay_const = 300.0;
+
 mms_mult = 100.00;
 u0 = 1.0;
-epsilon = 0.001;
-om = 1.0e6;
+epsilon = 1.0;
+om = 1.0e10;
 
 %-- initial density profile
 n_new = u0*(sin(mms_mult*nxax.^2) + epsilon);
@@ -96,7 +127,12 @@ n_avg = interp1(nxax,n_new,vxax);
 n_init = n_new;
 
 %-- initial velocity
-vx_new = u0*(sin(mms_mult*vxax.^2) + epsilon);
+% vx_new = u0*(sin(mms_mult*vxax.^2) + epsilon);
+% vx_mult = log(cs)/(xmax - xmin);
+% vx_const = -exp(vx_mult*xmin);
+% vx_new = vx_const + exp(vx_mult*vxax)*(0 + epsilon);
+% vx_new = 1.0e4*vx_new/max(vx_new);
+vx_new = exp(-decay_const*vxax)*(sin(0) + epsilon);
 vx_init = vx_new;
 
 
@@ -119,7 +155,8 @@ vx_I = sparse(eye(npts-1,npts-1));
 %-- set dt based on CFL conditions, check during loop if violated
 
 tmin = 0;
-tmax = 1.0e-5;
+% tmax = 1.0e-5;
+tmax = 20*dt;
 cfl_fact = 0.99;
 
 if ((cfl_fact*(min(ndx)^2)/(2.0*nu))<(cfl_fact*min(ndx)/max(abs(vx_new))))
@@ -133,3 +170,18 @@ end
 dt = 2.0*dt;
 nmax = round(tmax/dt);
 tax = linspace(tmin,tmax,nmax);
+
+for ii=1:20
+    vx_new = exp(-decay_const*vxax)*(sin(om*dt*ii) + epsilon);
+%     vx_new = 1.0e4*vx_new/max(vx_new);
+    figure(2)
+    plot(vxax,vx_new)
+    pause(2)
+    hold on
+end
+
+figure(2)
+hold on
+xlabel('Position (m)','Fontsize',16)
+ylabel('Exact solution','Fontsize',16)
+hold off
