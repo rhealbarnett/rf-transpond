bed3caa109b0b6f7e57cdc5517119691f4aa659b
rhealbarnett <rhealbarnett@gmail.com>
2019-01-31 16:48:54 -0500

    number of points now calculated up, using 2^n, rather than setting the max and halving each iteration

--- transport_mmsscript.m
+++ transport_mmsscript.m
@@ -1,15 +1,20 @@
-npts = 4096;
+% npts = 4096;
+pow = 3;
 
-for kk=1:5
+for kk=1:3
+    
+    npts = 2^pow;
     
     fprintf('Iteration %d\n',kk)
     fprintf('Number of points %d\n',npts)
     
     transport_1d
     
-    ltwo_arr(1,kk) = l_two;
-    linf_arr(1,kk) = l_inf;
+    ltwon_arr(1,kk) = l_twon;
+    linfn_arr(1,kk) = l_infn;
+    ltwou_arr(1,kk) = l_twou;
+    linfu_arr(1,kk) = l_infu;
     
-    npts = npts/2;
+    pow = pow + 1;
     
 end
