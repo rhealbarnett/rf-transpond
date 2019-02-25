85295e595d84d076a58cc0d8de87c994b735cd2d
rhealbarnett <rhealbarnett@gmail.com>
2019-02-25 10:52:21 -0500

    added atomic mass unit

--- constants.m
+++ constants.m
@@ -13,8 +13,11 @@ function const = constants()
     const.e = 1.6022e-19;
     const.mp = 1.67e-27;
     const.me = 9.11e-31;
+    const.amu = 1.66e-27;
     const.kb = 1.38e-23;
     const.mu0 = 4.0*pi*1.0e-7;
     const.eps0 = 8.85e-12;
     const.c0 = 1.0/sqrt(const.eps0*const.mu0);
+    
+    
 end
