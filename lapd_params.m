66d9e598d2fe743d22d9494d4f53d5d306cde67e
rhealbarnett <rhealbarnett@gmail.com>
2019-03-05 16:31:18 -0500

    changes to density and kperp to test wave solver

--- lapd_params.m
+++ lapd_params.m
@@ -18,13 +18,12 @@ mu0 = const.mu0;
 charge = [e; q];
 
 % magnetic field magnitude, 1000 G = 0.1 T
-% B0 = 0.1;
-B0 = 1.0;
+B0 = 0.1;
 
 % electron and ion temperatures, (eV)
 % multiply by e for temps in K
-Te = 1.0;
-Ti = 6.0;
+Te = 5.0;
+Ti = 0.5;
 
 % plasma column is ~ 18 (m) 
 xmin = -4.;
@@ -45,37 +44,37 @@ mhe = mhe*ones(1,npts);
 
 % DLG - since I don't have the license for "makedist"
 % I fixed your cos ramping function :)
-% dampFac = 5.0e1;
-% np_bound = floor(0.2*npts);
-% ax = linspace(0,pi,np_bound);
-% damp0 = (cos(ax)+1)/2;
-% damp = ones(1,npts);
-% damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
-% damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
-% 
-% me = me .* damp;
+dampFac = 10.0e1;
+np_bound = floor(0.2*npts);
+ax = linspace(0,pi,np_bound);
+damp0 = (cos(ax)+1)/2;
+damp = ones(1,npts);
+damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
+damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
+
+me = me .* damp;
 
 m = [me; mhe];
 
 % driving frequency of the single strap, high power antenna (Hz)
-freq = 24e6;
+freq = 2.4e6;
 % freq = 13.0e6;
 om = freq*2.0*pi;
 
 % electron density range is (1.0e17 <= n <= 7.9e18) (m^-3)
-Nmax = 1.0e20;
-Nmin = 1.0e14;
-n_new = logspace(log10(Nmin),log10(Nmax),npts);
-% n_new = Nmax*ones(1,npts);
+Nmax = 7.9e18;
+Nmin = 1.0e17;
+% n_new = logspace(log10(Nmin),log10(Nmax),npts);
+n_new = Nmin*ones(1,npts);
 
 % perpendicular wavenumber : just an approximation for now
 % see figure 10 in Martin 2016 poster for n_perp and
 % n_para, n = c0*k/om
-% n_perp = linspace(0,800,npts);
+n_perp = linspace(0,800,800);
 % n_perp = 
-% k_perp = om*n_perp./c0;
-k_perp = 20*ones(1,npts);
-n_perp = c0*k_perp./om;
+k_perp = om*n_perp./c0;
+% k_perp = 20*ones(1,npts);
+% n_perp = c0*k_perp./om;
 % wave_perp = 2.0*pi./k_perp;
 % k0 = om/c0;
 % wave0 = 2.0*pi/k0;
@@ -84,9 +83,10 @@ n_perp = c0*k_perp./om;
 % kx = 40.;
 
 ky = 0.;
-kz = 0.;
+kz = 5.;
 k0 = om/c0;
 
-source_width = 0.01;
+source_width = 0.03;
+source_loc = 0;
 
 
