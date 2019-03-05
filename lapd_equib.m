ccbc5c7fc17996901924e7fe1138f21aa5c997e9
rhealbarnett <rhealbarnett@gmail.com>
2019-03-05 11:39:16 -0500

    calls equilibrium profiles for inclusion of pf in transport model

---
+++ lapd_equib.m
@@ -0,0 +1,10 @@
+%------------------------------------------------------------------%
+% call equilibrium profiles for the lapd transport solve
+% rlbarnett c3149416 190304 
+%------------------------------------------------------------------%
+
+
+equib = load('../../lapd_numdata/matlab/equibhe_8m.mat');
+
+vxax = equib.vxax;
+vdx = equib.vdx;
