7ed6f1903b728ce41b10dc086e98b3e8936e2505
rhealbarnett <rhealbarnett@gmail.com>
2019-03-07 10:31:27 -0500

    added additional terms to velocity coefficient matrix for the density source term

--- transport_1d.m
+++ transport_1d.m
@@ -462,6 +462,7 @@ timerVal = tic;
 vx_rms = zeros(1,nmax);
 n_rms = zeros(1,nmax);
 
+
 for ii=1:nmax
     
     if MMS
@@ -585,9 +586,11 @@ for ii=1:nmax
             if vx(1,jj)>0
                 vx_pos(jj,jj) = - (1.0/vdx(1,jj-1))*vx(1,jj);
                 vx_pos(jj,jj-1) = (1.0/vdx(1,jj-1))*vx(1,jj);
+                vx_sn(jj,jj) = (1.0/n(1,jj-1))*n_source(1,jj-1);
             elseif vx(1,jj)<0
                 vx_neg(jj,jj) = (1.0/vdx(1,jj))*vx(1,jj);
                 vx_neg(jj,jj+1) = - (1.0/vdx(1,jj))*vx(1,jj);
+                vx_sn(jj,jj) = (1.0/n(1,jj))*n_source(1,jj);
             end
             vx_diff(jj,jj) = - (1.0/(vdx(1,jj-1)*vdx(1,jj)))*(2.0*nu);
             vx_diff(jj,jj-1) = (2.0/(vdx(1,jj-1)*(vdx(1,jj) + vdx(1,jj-1))))*nu;
@@ -599,7 +602,7 @@ for ii=1:nmax
         if upwind 
             vxA = vx_pos + vx_neg;
         elseif central
-            vxA = vx_pos + vx_neg + vx_diff;
+            vxA = vx_pos + vx_neg + vx_diff + vx_sn;
         end
 
         % build full coefficient matrices
