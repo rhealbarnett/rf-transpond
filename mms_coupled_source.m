4dbce2ee35593ca449f4e53612074f366a96b99b
rhealbarnett <rhealbarnett@gmail.com>
2019-04-10 16:00:41 -0400

    edited to check time independent velocity for continuity only testing

--- mms_coupled_source.m
+++ mms_coupled_source.m
@@ -1,7 +1,7 @@
 syms n0 u0 nx ux knx kux x lam om t 
 
 n = n0 + nx*sin(knx*x^2 + om*t);
-u = u0 + ux*cos(kux*x^2 + om*t);
+u = u0 + ux*cos(kux*x^2);
 
 % n = n0 + nx*cos(knx*x^2 + om*t)*exp(-lam*x);
 % u = u0 + ux*cos(kux*x^2 + om*t)*exp(-lam*x);
