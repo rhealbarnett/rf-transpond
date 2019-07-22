adef77fc38f65dd0d39e51b47419f65948e84dda
rhealbarnett <rhealbarnett@gmail.com>
2019-07-23 08:07:10 +1000

    included variable grid calculation

--- input_transport_1_5D.m
+++ input_transport_1_5D.m
@@ -16,7 +16,7 @@ e = const.e;
 % define parameters. %
 %-------
 Te = 5.0;
-Ti = 0.5;
+Ti = 1.0;
 cs = sqrt((Te + Ti)*e/m);
 
 eta_para = 1.0;
@@ -29,7 +29,129 @@ D_perp = 1.0;
 %-- 
 % Setting the perp derivatives as zero to begin, for comparison with the 
 % parallel only case. 
-kapx = 0.0;
-kapy = 0.0;
+kap_x = 0.0;
+kap_y = 0.0;
+
+%%
+%-------
+% Calculate perpendicular drift velocity from analytic expression
+% (in this case, PF only).
+%-------
+
+%--
+% Setting drift velocity as zero to begin, also for comparison with the
+% parallel only case. 
+vdrift_x = 0.0;
+vdrift_y = 0.0;
+
+%%
+%-------
+% Initialise the grid parameters.
+%-------
+
+npts = 512;
+xmin = -0.1;
+xmax = 0.1;
+
+variable = 0;
+
+%--------
+% Calculate variable grid OR keep uniform grid.
+%--------
+
+if variable
+    %-- 
+    % Find location of sign change.
+    xc = (xmax - xmin)/2.0;
+
+    %--
+    % Strength of grid refinement. Sign indicates whether refinement is in 
+    % the centre (positive) or at the boundaries (negative).
+    A = -5.0;
+
+    %--
+    % set up the unit spaced parameter, s, that the grid is a function of.
+    smax = 1.0;
+    smin = 0.0;
+    s = linspace(smin,smax,npts-1); 
+
+    %--
+    % Calculate the grid values from s. 
+    x = xc*(1.0 - tanh(A*(1.0 - 2.0*s))./tanh(A));
+
+    %--
+    % Calculate the velocity grid & grid spacing from the calculated grid above. 
+    vxax = x - xmax;
+    vdx = (vxax(2:end) - vxax(1:end-1));
+
+    %--
+    % Initialise density grid.
+    nxax = zeros(1,npts);
+
+    %--
+    % Fill first (last) index with value calculated from first (last) velocity 
+    % grid value. 
+    nxax(1) = vxax(1) - 0.5*vdx(1);
+    nxax(end) = vxax(end) + 0.5*vdx(end);
+
+    %--
+    % Calculate the density grid as halfway between velocity grid points. Also
+    % calculate density grid spacing. 
+    nxax(2:end-1) = (vxax(2:end) + vxax(1:end-1))/2.0;
+    ndx = nxax(2:end) - nxax(1:end-1);
+    
+elseif ~variable
+    
+    %-- 
+    % Calculate grid spacing.
+    dx = (xmax - xmin)/(npts - 1);
+    
+    %-- 
+    % Define density and velocity grids.
+    nxax = linspace(xmin-0.5*dx,xmax+0.5*dx,npts);
+    vxax = linspace(xmin,xmax,npts-1);
+    
+    %-- 
+    % Calculate uniform spaced grid. Needs to have this form to maintain
+    % consistency with the transport solve itself.
+    ndx = dx*ones(1,npts-1);
+    vdx = dx*ones(1,npts-2);
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
