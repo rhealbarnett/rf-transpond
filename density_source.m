9781b9062a71368aa7cb291dbc437acc75430ef2
rhealbarnett <rhealbarnett@gmail.com>
2019-07-23 12:05:37 +1000

    Included more function inputs as needed; commented each step to provide clarity

--- density_source.m
+++ density_source.m
@@ -1,50 +1,106 @@
-%%
 %-------------------------------------------------------------------------%
-% CALCULATE DENSITY SOURCE                                                %
+% Calculate density source term for RHS of continuity equation            %
+%
+% density_source(rate_coeff, fact, nxax, vxax, npts, neut_max, vx_new,
+%                   n_new)
+%
+% INPUTS
+% rate_coeff: ionsation rate coefficient 
+%
+% fact: percentage of the domain where the neutral density profile is
+% non-zero for one boundary ONLY e.g. if you enter 0.05, this will be 5%
+% of the total domain on either side (for 10% total across the whole
+% domain). 
+%
+% nxax, vxax, npts: density and velocity grids, number of points.
+%
+% neut_max: maximum value for the neutral density profile.
+%
+% vx_new, n_new: velocity and density profiles used to calculate the
+% neutral source term, fluxes at boundaries. 
 %-------------------------------------------------------------------------%
-% 
 
-function [ans] = density_source(rate_coeff,nxax,vxax)
+function [n_source] = density_source(rate_coeff,fact,nxax,vxax,npts,neut_max,vx_new,n_new)
 
-    % rate coefficient (constant)
-    rate_coeff = (1.0e-14);
-    % approx size of non-zero portion of neutral profile (1/4 domain)
-    decay_loc = xmax - 0.0001*xmax;
-    % decay_loc = xmax - 0.2*xmax;
+    %--
+    % Get maximum grid value from the density grid. 
+    xmax = max(vxax);
+    xmin = min(vxax);
+    dx = (xmax - xmin)/(npts-1);
+
+    %--
+    % Approximate size of non-zero portion of neutral profile.
+    decay_loc = xmax - fact*xmax;
+    
+    %--
+    % Determine the index for this density location. 
     a = find(nxax >= decay_loc);
+    
+    %--
+    % Determine the number of points included in the non-zero region of the
+    % neutral density. 
     decay_index = npts - a(1);
-
-    % decay_index = round((npts)/4);
-    % calculate shape of neutral profile
-    cosax = linspace(pi,2*pi,decay_index);
-    % max neutral value (at wall)
-    neut_max = (1.0e18);
-
-    % initialise and fill neutral density array
+    
+    %--
+    % Initialise the enutral density array. 
     n_neut = zeros(1,npts);
+
+    %-- 
+    % Calculate shape of neutral profile. Approximated as half a cosine
+    % wave.
+    cosax = linspace(pi,2*pi,decay_index);
     n_neut(end-decay_index+1:end) = neut_max*((cos(cosax) + 1)/2);
-    % n_neut(1:end-decay_index+1) = n_neut(end-decay_index+2);
+
+    %--
+    % Fill the remaining half of the array with the inner most calculated 
+    % value for the neutral density.
     n_neut(end/2 + 1:end-decay_index+1) = neut_max*((cos(pi) + 1)/2);
+    
+    %--
+    % For two metallic boundaries, mirror around the centre. 
     n_neut(1,1:end/2) = fliplr(n_neut(end/2 + 1:end));
+    
+    %--
+    % Interpolate onto the variable density grid.
     n_neut = interp1(linspace(xmin-0.5*dx,xmax+0.5*dx,npts),n_neut,nxax,'linear');
 
-    % calculate density source
-    n_source = (n_new.*(n_neut)*(rate_coeff));
+    %--
+    % Calculate the density source.
+    n_source = n_new.*(n_neut)*(rate_coeff);
 
-    % interpolate source onto velocity grid
-    source_avg = interp1(nxax,n_source,vxax);
-    % calculate integral of density source over the velocity grid
+    %--
+    % Interpolate source onto velocity grid to calculate the flux at the
+    % boundary.
+    source_avg = interp1(nxax,n_source,vxax,'linear');
+    
+    %--
+    % Interpolate the densities onto the velocity grid. 
+    n_avg = interp1(nxax,n_new,vxax);
+    
+    %--
+    % Calculate the integral of the density source over the velocity grid.
     source_int = trapz(vxax,source_avg);
-    % source_int = trapz(nxax,n_source);
-    % calculate flux at the rh boundary (wall)
-    % rflux = n_avg(end)*vx_new(end);
+
+    %--
+    % Calculate the fluxes at both boundaries. 
     rflux = n_avg(end)*vx_new(end);
     lflux = n_avg(1)*vx_new(1);
-    % calculate the constant multiplier to match density out = in
+    
+    %--
+    % Calculate the multiplier to match density out = density in. 
     ns_mult = (rflux-lflux)/source_int;
-    % multiply n0(x)n(x,t) by the constant calculated in previous step
+    
+    %--
+    % Multiply the density source term by the constant ns_mult. 
     n_source = (n_source*ns_mult)*1.0e-2;
-    nv_source = source_avg*ns_mult;
+    
+    %--
+    % Calculate the new 'normalised' source term on the velocity grid. 
+    nv_source = source_avg*ns_mult*1.0e-2;
+    
+    %--
+    % Set the ghost point values for the density source to zero, as they
+    % are not used in the calculation. 
     n_source(1,1) = 0.0; n_source(1,end) = 0.0;
 
 end
