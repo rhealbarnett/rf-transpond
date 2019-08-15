a2c298969c098ab451ea23197a912a87f70fdc86
rhealbarnett <rhealbarnett@gmail.com>
2019-08-15 10:31:47 +1000

    Edited to play with imaginary mass component.

--- dielec_tens.m
+++ dielec_tens.m
@@ -18,7 +18,7 @@ function [om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q,B0,n,m,om,eps0,npts)
     msize = size(m);
 
     for ii=1:msize(1)
-        om_p(ii,:) = plasma_freq(q(ii,:),n,m(ii,:),eps0);
+        om_p(ii,:) = plasma_freq(q(ii,:),n,real(m(ii,:)),eps0);
         om_c(ii,:) = cyclo_freq(q(ii,:),B0,m(ii,:));
     end
 
