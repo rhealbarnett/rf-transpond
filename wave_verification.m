4bb8eaf4c0986495ad51ddbbbb04f4f65ceb6369
rhealbarnett <rhealbarnett@gmail.com>
2019-08-15 10:32:55 +1000

    Reduced number of points to make looking at the full coefficient matrix less painful on the eyeballs.

--- wave_verification.m
+++ wave_verification.m
@@ -22,11 +22,16 @@ charge = [e; q];
 % Magnetic field magnitude.
 B0 = 2.5;
 
+%--
+% Driving frequency in the MHz range
+freq = 51.0e6;
+om = freq*2.0*pi;
+
 %-- 
 % Set some domain size and length
-xmin = -4.;
-xmax = 4.0;
-npts = 512;
+xmin = -8.;
+xmax = 8.0;
+npts = 8;
 xax = linspace(xmin,xmax,npts);
 
 %--
@@ -36,37 +41,37 @@ mp = mp*ones(1,npts);
 
 % DLG - since I don't have the license for "makedist"
 % I fixed your cos ramping function :)
-% dampFac = 25.0e1;
-% np_bound = floor(0.2*npts);
-% ax = linspace(0,pi,np_bound);
-% damp0 = (cos(ax)+1)/2;
-% damp = ones(1,npts);
-% damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
-% damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
-% 
-% me = me .* damp;
+dampFac = 50.0e1;
+np_bound = floor(0.2*npts);
+ax = linspace(0,pi,np_bound);
+damp0 = (cos(ax)+1)/2;
+damp = ones(1,npts);
+damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
+damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
 
+% m_s = [me; mp].*damp;
 m_s = [me; mp];
-
-%--
-% Driving frequency in the MHz range
-freq = 51.0e6;
-om = freq*2.0*pi;
 
 %--
 % Scan over density values.
 Nmax = 1.0e20;
 Nmin = 1.0e15;
-% n_new = logspace(log10(Nmin),log10(Nmax),npts);
-n_new = 1.0e19*ones(1,npts);
+n_new = logspace(log10(Nmin),log10(Nmax),npts);
+n_new = 0*ones(1,npts);
 
 %--
 % Calculate free space wavenumber and set perpendicular (or parallel) 
 % wave number. 
 k0 = om/c0;
 k_para = 5.0;
+n_para = k_para*c0/om;
+% n_para = linspace(0,800,800);
+% k_para = om*n_para/c0;
+% k_perp = 17;
+% n_perp = k_perp*c0/om;
 ky = 0.;
 kz = k_para;
+
 
 %--
 % Set current source parameters. 
