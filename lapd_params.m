aec7c76001a0a6787d6acfc92f831740d512d55a
rhealbarnett <rhealbarnett@gmail.com>
2019-09-11 17:33:52 -0400

    Changed the damping factor for the imaginary component of mass.

--- lapd_params.m
+++ lapd_params.m
@@ -48,19 +48,19 @@ mhe = mhe*ones(1,npts);
 % Electron mass.
 me = me*ones(1,npts);
 
-
 %--
 % Imaginary component of the mass is used to damp any waves near the
 % boundary. 
-% dampFac = 25.0e1;
-% np_bound = floor(0.2*npts);
-% ax = linspace(0,pi,np_bound);
-% damp0 = (cos(ax)+1)/2;
-% damp = ones(1,npts);
-% damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
-% damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
-% 
-% me = me .* damp;
+dampFac = 20.e-1;
+np_bound = floor(0.2*npts);
+ax = linspace(0,pi,np_bound);
+damp0 = (cos(ax)+1)/2;
+damp = ones(1,npts);
+damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
+damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
+
+me = me .* damp;
+mhe = mhe .* damp;
 
 %--
 % Collect masses in 2d array for dielectric tensor calculation. 
@@ -72,23 +72,27 @@ m_s = [me; mhe];
 % ~2.52MHz. 
 freq = 2.52e6;
 om = freq*2.0*pi;
+% coll = 0.1;
+% om = om + coll*om*1i;
+% om = om.*damp;
 
 %--
 % Electron density range is (1.0e17 <= n <= 7.9e18) (m^-3). Scan over these
 % values, +- some amount. 
 Nmax = 1.0e19;
 Nmin = 1.0e16;
-n_new = logspace(log10(Nmin),log10(Nmax),npts);
-% n_new = Nmin*ones(1,npts);
+% n_new = logspace(log10(Nmin),log10(Nmax),npts);
+n_new = Nmax*ones(1,npts);
 
 %--
 % Wavenumber in x approximated using experimental data, kx ~ (0 + 20i)
-% m^-1. Was more difficult to obtain ky from the data, should perhaps use 
-% a larger range to scan over. 
+% m^-1. Difficult to tell whether wavenumber in y is also imaginary, but
+% the value is ~ 36 m^-1. 
 k0 = om/c0;
 kx = 20.0i;
 % ky = 36.0i;
-ky = linspace(0,40,100);
+% ky = linspace(0,40,100);
+ky = 0;
 k_perp = sqrt(kx.^2 + ky.^2); 
 n_perp = c0*k_perp./om;
 
