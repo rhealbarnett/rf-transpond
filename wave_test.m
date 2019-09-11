1f1b08a619105e275e7fb54bd0faa0a7cdd89086
rhealbarnett <rhealbarnett@gmail.com>
2019-09-11 17:38:14 -0400

    Deleted clear and close at the top of the script.

--- wave_test.m
+++ wave_test.m
@@ -3,7 +3,6 @@
 % parameters. 
 %-----------------------------------------%
 
-clear all; close all;
 lapd_params;
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(charge,B0,n_new,m_s,om,eps0,npts);
 [A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
