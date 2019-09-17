789496891b344dee7b806aa4978aba6a83ec1afe
rhealbarnett <rhealbarnett@gmail.com>
2019-09-17 11:28:49 -0400

    Added the new source multiplier value to the wave solver function call.

--- wave_test.m
+++ wave_test.m
@@ -6,5 +6,5 @@
 lapd_params;
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(charge,B0,n_new,m_s,om,eps0,npts);
 [A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
-om,mu0,cpdt,source_width,source_loc,0,1,1);
+om,mu0,cpdt,source_width,source_loc,0,1.0e5,1);
 wave_sol_plots
