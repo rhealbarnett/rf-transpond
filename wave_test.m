e03f3d843d86df542d4f69c648397af6d45c8dd1
rhealbarnett <rhealbarnett@gmail.com>
2019-10-17 11:35:29 +1100

    Edited to add / test inclusion of ramped down kx and ky profile in absorbing boundary regions.

--- wave_test.m
+++ wave_test.m
@@ -4,7 +4,7 @@
 %-----------------------------------------%
 
 lapd_params;
-[om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(charge,B0,n_new,m_s,om,eps0,npts);
+[om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
 [A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
-om,mu0,cpdt,source_width,source_loc,0,1.0e5,1);
+om,mu0,cpdt,source_width,source_loc,0,1.0e4,1);
 wave_sol_plots
