6ab8a5075302ab3ce0e37d9a558347c77606e67e
rhealbarnett <rhealbarnett@gmail.com>
2019-10-29 13:09:56 +1100

    Included ramped imaginary frequency profile in boundary regions.

--- dielec_tens.m
+++ dielec_tens.m
@@ -17,18 +17,28 @@ function [om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q,B0,n,m,om,eps0,npts)
     
     msize = size(m);
     nsize = size(n);
+    
+    dampFac = 0.1;
+    np_bound = floor(0.2*npts);
+    ax = linspace(0,pi,np_bound);
+    damp0 = (cos(ax)+1)/2;
+    damp = ones(1,npts);
+    damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
+    damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
+    om = om*ones(1,npts);
+    om = om.*damp;
 
     for ii=1:msize(1)
         if nsize(1)~=1
             om_p(ii,:) = plasma_freq(q(ii,:),n(ii,:),(m(ii,:)),eps0);
         else
-            om_p(ii,:) = plasma_freq(q(ii,:),n,real(m(ii,:)),eps0);
+            om_p(ii,:) = plasma_freq(q(ii,:),n,(m(ii,:)),eps0);
         end
         om_c(ii,:) = cyclo_freq(q(ii,:),B0,(m(ii,:)));
     end
 
     s = 1.0 - sum((om_p.^2)./(om.^2 - om_c.^2),1);
-    d = sum(om_c.*om_p.^2./(om.*(om.^2 - om_c.^2)),1);
+    d = sum((om_c).*(om_p).^2./((om).*((om).^2 - (om_c).^2)),1);
     p  = 1.0 - sum((om_p.^2/om.^2),1);
 
     s_arr(1,:) = s;
