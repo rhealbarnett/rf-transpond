8212c9d849a408c11e7f93fbf1f1683cc14c98ac
rhealbarnett <rhealbarnett@gmail.com>
2019-11-06 13:23:36 +1100

    Switching between new and old matrix filling schemes to compare speed.

--- wave_test.m
+++ wave_test.m
@@ -3,8 +3,8 @@
 % parameters. 
 %-----------------------------------------%
 
-lapd_params;
-[om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
-[A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
-om,mu0,cpdt,source_width,source_loc,0,1.0e4,1);
-wave_sol_plots
+lapd_params
+[om_c,om_p,cpdt,s_arr,d_arr,p_arr,sigma] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
+[A,source,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(xax,ky,kx,k0,...
+om,mu0,cpdt,sigma,ey_source,R,0,1,0);
+% wave_sol_plots
