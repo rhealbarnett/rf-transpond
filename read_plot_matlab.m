101d10ed9640a72a56f7e3edd7a79f998713b2a3
rhealbarnett <rhealbarnett@gmail.com>
2019-11-06 13:56:52 +1100

    Deleted the appending of wave solver parameters.

--- read_plot_matlab.m
+++ read_plot_matlab.m
@@ -7,22 +7,9 @@
 
 filepath = '/Volumes/DATA/LAPD/matlab/results_jsource_kyzero/';
 
-lapd_equib;
-
-for ii=212:106:47382
+for ii=106:106:47382
     
     filename = strcat(filepath, 'coupled_transport_', num2str(ii),'.mat');
-    
-    transport.freq = freq;
-    transport.period = period;
-    transport.B0 = B0;
-    transport.source_dist = source_dist;
-    transport.scale_fact = scale_fact;
-    transport.ey_source = ey_source;
-    transport.R = R;
-    transport.kx = kx;
-    transport.ky = ky;
-    
-    save(filename,'-struct','transport','-append');
+
     
 end
