d9083c584ed695e887463d1dc8e3f2ce1306d05a
rhealbarnett <rhealbarnett@gmail.com>
2019-11-06 13:55:09 +1100

    Added loop to append wave solver parameters to saved transport parameters in each file.

--- read_plot_matlab.m
+++ read_plot_matlab.m
@@ -6,7 +6,23 @@
 % ----------------------------------------------------------------------- %
 
 filepath = '/Volumes/DATA/LAPD/matlab/results_jsource_kyzero/';
-filename = 'coupled_transport_106.mat';
 
-load([filepath filename]);
+lapd_equib;
 
+for ii=212:106:47382
+    
+    filename = strcat(filepath, 'coupled_transport_', num2str(ii),'.mat');
+    
+    transport.freq = freq;
+    transport.period = period;
+    transport.B0 = B0;
+    transport.source_dist = source_dist;
+    transport.scale_fact = scale_fact;
+    transport.ey_source = ey_source;
+    transport.R = R;
+    transport.kx = kx;
+    transport.ky = ky;
+    
+    save(filename,'-struct','transport','-append');
+    
+end
