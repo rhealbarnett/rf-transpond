c8c2c0402a5115b3f952e503d6808954f2939f9e
rhealbarnett <rhealbarnett@gmail.com>
2019-11-06 13:30:14 +1100

    Script to read and plot matlab files from coupled 1d transport / wave solver.

---
+++ read_plot_matlab.m
@@ -0,0 +1,12 @@
+% ----------------------------------------------------------------------- %
+% Script to read and plot matlab (.mat) files for the coupled 1D 
+% wave / transport solver. 
+%
+% rlbarnett c3149416 191106
+% ----------------------------------------------------------------------- %
+
+filepath = '/Volumes/DATA/LAPD/matlab/results_jsource_kyzero/';
+filename = 'coupled_transport_106.mat';
+
+load([filepath filename]);
+
