92bb66933c1d5cd53373aaf029a5b3af3251e910
rhealbarnett <rhealbarnett@gmail.com>
2019-11-12 10:01:19 +1100

    Edited function inputs and outputs to reflect e field source changes.

--- wave_test.m
+++ wave_test.m
@@ -5,6 +5,7 @@
 
 lapd_params;
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
-[A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
-om,mu0,cpdt,source_width,source_loc,0,1.0e4,1);
+[ey_source,R] = e_source(xax,scale_fact,source_dist);
+[A,source,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(xax,ky,kx,k0,...
+    om,mu0,cpdt,sigma,ey_source,R,0,1);
 wave_sol_plots
