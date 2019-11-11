1ddc7484021eee5d089bd95eb0ec1e6b3f51e2d5
rhealbarnett <rhealbarnett@gmail.com>
2019-11-12 09:50:31 +1100

    Edited cold plasma dielectric tensor elements to contain the Krook model collision terms in the boundaries.

--- dielec_tens.m
+++ dielec_tens.m
@@ -18,15 +18,15 @@ function [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sigma] = dielec_tens(q,B0,n,m,om,eps0
     msize = size(m);
     nsize = size(n);
     
-%     dampFac = 1.0;
-%     np_bound = floor(0.2*npts);
-%     ax = linspace(0,pi,np_bound);
-%     damp0 = (cos(ax)+1)/2;
-%     damp = ones(1,npts);
-%     damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
-%     damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
+    dampFac = 1.0;
+    np_bound = floor(0.2*npts);
+    ax = linspace(0,pi,np_bound);
+    damp0 = (cos(ax)+1)/2;
+    damp = ones(1,npts);
+    damp(1:np_bound) = damp(1:np_bound) + dampFac*1i*damp0;
+    damp(end-np_bound+1:end) = damp(end-np_bound+1:end) + dampFac*1i*fliplr(damp0);
     om = om*ones(1,npts);
-%     om = om.*damp;
+    om = om.*damp;
 
     for ii=1:msize(1)
         if nsize(1)~=1
@@ -37,9 +37,9 @@ function [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sigma] = dielec_tens(q,B0,n,m,om,eps0
         om_c(ii,:) = cyclo_freq(q(ii,:),B0,(m(ii,:)));
     end
 
-    s = 1.0 - sum(((om_p).^2)./((om).^2 - (om_c).^2),1);
-    d = sum((om_c).*(om_p).^2./((om).*((om).^2 - (om_c).^2)),1);
-    p  = 1.0 - sum(((om_p).^2./(om).^2),1);
+    s = 1.0 - sum(((om_p).^2)./(real(om).*(om - (om_c).^2)),1);
+    d = sum((om_c).*(om_p).^2./(real(om).*((om).^2 - (om_c).^2)),1);
+    p  = 1.0 - sum(((om_p).^2./(real(om).*om)),1);
 
     s_arr(1,:) = s;
     d_arr(1,:) = d;
