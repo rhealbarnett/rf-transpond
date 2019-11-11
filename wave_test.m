064aca8b4ce4a48dffd9652f49d475da116ae17b
rhealbarnett <rhealbarnett@gmail.com>
2019-11-12 10:04:50 +1100

    Included sig output from call to dielectric tensor function.

--- wave_test.m
+++ wave_test.m
@@ -4,7 +4,7 @@
 %-----------------------------------------%
 
 lapd_params;
-[om_c,om_p,cpdt,s_arr,d_arr,p_arr] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
+[om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts);
 [ey_source,R] = e_source(xax,scale_fact,source_dist);
 [A,source,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(xax,ky,kx,k0,...
     om,mu0,cpdt,sig,ey_source,R,0,1);
