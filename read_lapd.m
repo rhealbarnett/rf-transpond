d4e259f100b82c5309423316d31bcfb11114d6ef
rhealbarnett <rhealbarnett@gmail.com>
2019-11-22 15:21:33 +1100

    Edits to try and untangle the plasma profile data file.

--- read_lapd.m
+++ read_lapd.m
@@ -1,17 +1,18 @@
 function read_lapd()
 
-file_name = "/Volumes/DATA/LAPD/RF_April22/12_Bdot_p30xyz_12kV.hdf5";
+file_name = "/Volumes/DATA/LAPD/RF_April22/100_BaPSF_check_plasma_profile.hdf5";
 
 info = h5info(file_name);
 
 % data = info.Groups(2).Groups(3).Datasets(5);
 
 % ch6_name = '/Raw data + config/SIS crate/Mach_antennacurrent_longertime [Slot 7: SIS 3302 ch 6]';
-ch6_name = '/Raw data + config/SIS crate/Bdot_only_antenna [Slot 5: SIS 3302 ch 1]';
+% ch6_name = '/Raw data + config/SIS crate/Bdot_only_antenna [Slot 5: SIS 3302 ch 1]';
+% ch6_name = '/Raw data + config/SIS crate/Isat [Slot 5: SIS 3302 ch 1]';
 
 
-disp('other channel names are ...');
-
+% disp('other channel names are ...');
+% 
 % for i=1:numel(data)
 %     
 %     disp(data(i).Name);
@@ -35,7 +36,7 @@ ch6_data = h5read(file_name,ch6_name, [startX startY], [nX nY]);
 
 % to get all the data for that channel simply use ...
 
-% ch6_data = h5read(file_name,ch6_name);
+ch6_data = h5read(file_name,ch6_name);
 
 
 
