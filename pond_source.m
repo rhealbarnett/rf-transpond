a4c5b71e5170be62c61ca7bb0dd6d9aa6554607c
rhealbarnett <rhealbarnett@gmail.com>
2019-12-04 16:23:42 +1100

    Included switches to calculate pf based on species and component of E field.

--- pond_source.m
+++ pond_source.m
@@ -19,18 +19,73 @@
 %
 %------------------------------------------------------------------%
 
-function [ans] = pond_source(m,mp,omega,q,Efield,dx,varargin)
-    for ii = 2:length(dx)
-        Ediff(1,ii-1) = -(dx(1,ii)/(dx(1,ii-1)*(dx(1,ii) + dx(1,ii-1))))*Efield(1,ii-1) +...
-            ((dx(1,ii) - dx(1,ii-1))/(dx(1,ii)*dx(1,ii-1)))*Efield(1,ii) +...
-            (dx(1,ii-1)/(dx(1,ii)*(dx(1,ii) + dx(1,ii-1))))*Efield(1,ii+1);
+function [ans] = pond_source(component,species,Efield,m,q,om_cyc,omega,npts,dz)
+
+    Esquared = [Efield{1}.^2; Efield{2}.^2; Efield{3}.^2];
+    Ediff = zeros(numel(Efield),npts);
+    
+    for jj = 1:numel(Efield)
+        for ii = 2:length(dx)
+            Ediff(jj,ii-1) = -(dz(1,ii)/(dz(1,ii-1)*(dz(1,ii) + dz(1,ii-1))))*Esquared(jj,ii-1) +...
+                ((dz(1,ii) - dz(1,ii-1))/(dz(1,ii)*dz(1,ii-1)))*Esquared(jj,ii) +...
+                (dz(1,ii-1)/(dz(1,ii)*(dz(1,ii) + dz(1,ii-1))))*Esquared(jj,ii+1);
+        end
     end
     
-    if numel(varargin)==0
-        pond_const = (1.0/4.0)*((q^2)/(m*omega^2));
-    else
-        pond_const = (1.0/4.0)*((q^2)/(m*(omega^2 - varargin{1}^2)));
+    if strcmp(component,'full')
+        
+        para = 1;
+        perp = 1;
+        
     end
     
-    ans = (1.0/mp)*pond_const*Ediff;
+    if strcmp(species,'total')
+        
+        electrons = 1;
+        ions = 1;
+        
+    end
+
+    if strcmp(species,'electrons') || electrons
+    
+        pf_const = q(1,1)^2/(4.0*m(1,1));
+        
+        if strcmp(component,'para') || para
+
+                pond_para(1,:) = (pf_const/omega^2)*Ediff(3,:);
+                
+        elseif strcmp(component,'perp') || perp
+
+                pond_perp(1,:) = (pond_const)*(((Ediff(1,:) + Ediff(2,:))/(omega^2 - om_cyc(1,1)^2))...
+                    + (om_cyc(1,1)/(omega*(omega^2 - om_cyc(1,1)^2)))*imag((conj(rf_ey)*rf_ex)...
+                    -(conj(rf_ex)*rf_ey)));
+
+        end
+        
+    elseif strcmp(species,'ions') || ions
+
+        pf_const = q(2,1)^2/(4.0*m(2,1));
+
+        if strcmp(component,'para') || para
+
+            pond_para(2,:) = (pf_const/omega^2)*Ediff(3,:);
+
+        elseif strcmp(component,'perp') || perp
+
+            pond_perp(2,:) = (pond_const)*(((Ediff(1,:) + Ediff(2,:))/(omega^2 - om_cyc(2,1)^2))...
+                    + (om_cyc(2,1)/(omega*(omega^2 - om_cyc(2,1)^2)))*imag((conj(Efield{2})*Efield{1})...
+                    -(conj(Efield{1})*Efield{2})));
+
+        end
+
+    end
+        
+    if perp && para
+        
+        pond = sum(pond_para,1) + sum(pond_perp,1);
+        
+    elseif 
+        
+
+    ans = (1.0/m(2,1))*pond;
 end
