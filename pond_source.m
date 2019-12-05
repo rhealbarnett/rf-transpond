36b5a10f369061d84de90845b8b5dbcb8875a225
rhealbarnett <rhealbarnett@gmail.com>
2019-12-05 13:41:14 +1100

    Fixed bug. Still need to check output is correct.

--- pond_source.m
+++ pond_source.m
@@ -1,21 +1,39 @@
 %------------------------------------------------------------------%
 % Function calculates ponderomotive acceleration
-% rlbarnett c3149416 updated 191127.
+% rlbarnett c3149416 updated 191205.
 %
 % INPUTS
-% m: mass of the particle being accelerated.
-% mp: proton mass from the velocity update equation.
-% omega: driving frequency for the fast E field.
-% q: charge of the particle being accelerated.
-% Efield: absolute value of the fast electric field squared, the quantity
-%       to be differentiated.
-% dx: array of dx values. 
-% varargin: option to include cyclotron frequency required for calculation
-%       of the ion ponderomotive acceleration.
+% component: string input, options are 'para', 'perp' or 'total'. This 
+%            will set which component of the electric field is used in the 
+%            calculation of the parallel ponderomotive acceleration.
+% Efield: {ex, ey, ez}. Can enter empty string if a particular component
+%         of the field is not being used e.g. {ex, ey, ''}.
+% m: mass. If multiple species, enter as column array e.g. [me; mi] for
+%    electron and ion mass. 
+% q: charge. If multiple species, enter as column array e.g. [e; q] for
+%    electron and ion charge. Position in the array needs to be consistent
+%    with position in the mass array.
+% om_cyc: cyclotron frequency. If multiple species, enter as column array 
+%         e.g. [om_ce; om_ci] for electron and ion frequencies. Position 
+%         in the array needs to be consistent with position in charge and 
+%         mass arrays.
+% omega: driving frequency for RF field. 
+% npts: number of points in the spatial domain. 
+% dz: grid spacing. Needs to be an array of size (1,npts). If dz is 
+%     constant over the domain, enter as same value at each location.
 %
 % OUPUTS
-% ans: the ponderomotive acceleration for input into an eq of motion, the 
-%       velocity equation.
+% ans: cell array of ponderomotive acceleration for input into an eq of 
+%      motion, the velocity equation. 2x2 cell, array of spatial values in
+%      each cell. Parallel component is stored in row index 1, 
+%      perpendicular in row index 2. Returns 0x0 double array in cell if 
+%      particular component is not calculated. Column index is dictated by 
+%      index of species input. E.g. for m = [me; mi], q = [e; q] and 
+%      om_cyc = [om_ce; om_ci], 'total' output is
+%
+%       2x2 cell array
+%       {PA Epara, e 1xnpts double}  {PA Epara, i 1xnpts double}
+%       {PA Eperp, e 1xnpts double}  {PA Eperp, i 1xnpts double}
 %
 %------------------------------------------------------------------%
 
@@ -33,7 +51,7 @@ function [ans] = pond_source(component,Efield,m,q,om_cyc,omega,npts,dz)
         end
     end
     
-    if strcmp(component,'full')
+    if strcmp(component,'total')
         
         para = 1;
         perp = 1;
@@ -43,29 +61,35 @@ function [ans] = pond_source(component,Efield,m,q,om_cyc,omega,npts,dz)
     msize = size(m);
     
     pf_const = zeros(msize(1),1);
-    pond = {[],msize(1)};
-%     pond_para = zeros(msize,npts);
-%     pond_perp = zeros(msize,npts);
+    pond = {};
     
     for ii=1:msize(1)
     
         pf_const(ii,1) = q(ii,1)^2/(4.0*m(ii,1));
         
         
-        if strcmp(component,'para') || para
+        if strcmp(component,'para')
 
-                pond{1,ii} = (pf_const(ii,1)/omega^2).*Ediff(3,:);
+            pond{1,ii} = (pf_const(ii,1)/omega^2).*Ediff(3,:);
                 
-        elseif strcmp(component,'perp') || perp
+        elseif strcmp(component,'perp')
 
-                pond{2,ii} = (pf_const(ii,1)).*(((Ediff(1,:) + Ediff(2,:))/(omega^2 - om_cyc(1,1)^2))...
-                    + (om_cyc(ii,1)/(omega*(omega^2 - om_cyc(ii,1)^2)))*imag((conj(Efield{2})*Efield{1})...
-                    -(conj(Efield{1})*Efield{2})));
+            pond{2,ii} = (pf_const(ii,1)).*(((Ediff(1,:) + Ediff(2,:))/(omega^2 - om_cyc(1,1)^2))...
+                    + (om_cyc(ii,1)/(omega*(omega^2 - om_cyc(ii,1)^2)))*imag((conj(Efield{2}).*Efield{1})...
+                    -(conj(Efield{1}).*Efield{2})));
+                
+        elseif strcmp(component,'total')
+            
+             pond{1,ii} = (pf_const(ii,1)/omega^2).*Ediff(3,:);
+             pond{2,ii} = (pf_const(ii,1)).*(((Ediff(1,:) + Ediff(2,:))/(omega^2 - om_cyc(1,1)^2))...
+                    + (om_cyc(ii,1)./(omega*(omega^2 - om_cyc(ii,1)^2))).*imag((conj(Efield{2}).*Efield{1})...
+                    -(conj(Efield{1}).*Efield{2})));
 
         end
         
     end
         
 
-    ans = (1.0/m(2,1))*pond;
+%     ans = (1.0/m(2,1))*pond;
+    ans = pond;
 end
