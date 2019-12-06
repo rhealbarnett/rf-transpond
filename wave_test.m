ca785e94c627b50079e6a2fc8fb013338f52b5a3
rhealbarnett <rhealbarnett@gmail.com>
2019-12-06 13:35:06 +1100

    Switched to equilibrium script call.

--- wave_test.m
+++ wave_test.m
@@ -3,8 +3,8 @@
 % parameters. 
 %-----------------------------------------%
 
-lapd_params;
+lapd_equib;
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts,1);
-[A,source,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(xax,ky,kx,k0,...
-om,mu0,cpdt,source_width,source_loc,0,1,1);
+[A,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(xax,ky,kx,k0,...
+om,mu0,cpdt,source,0,1,1);
 wave_sol_plots
