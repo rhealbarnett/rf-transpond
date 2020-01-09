7a97bc444cbfa81394bd6472382f128bdc2b8881
rhealbarnett <rhealbarnett@gmail.com>
2020-01-10 07:56:33 +1100

    Function to calculate the solution from the RF wave solver onto the ignorable directions.

---
+++ wave_projection.m
@@ -0,0 +1,44 @@
+%-------------------------------------------------------------------------%
+% Projection of wave solution into ignorable directions. 
+% rlbarnett c3149416 200109
+%-------------------------------------------------------------------------%
+
+function [Ex_x, Ey_x, Ez_x, Ex_y, Ey_y, Ez_y] = wave_projection(xax,yax,kx,ky,rf_ex,rf_ey,rf_ez,npts)
+
+    nptsx = numel(xax);
+    nptsy = numel(yax);
+
+    Ex_x = zeros(npts,nptsx);
+    Ex_y = zeros(npts,nptsy);
+    Ey_x = zeros(npts,nptsx);
+    Ey_y = zeros(npts,nptsy);
+    Ez_x = zeros(npts,nptsx);
+    Ez_y = zeros(npts,nptsy);
+    
+    for jj=1:npts
+        for ii=1:nptsx
+
+            if xax(1,ii)<0
+                xax(1,ii) = -1*xax(1,ii);
+            end
+
+            Ex_x(jj,ii) = rf_ex(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
+            Ey_x(jj,ii) = rf_ey(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
+            Ez_x(jj,ii) = rf_ez(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
+
+        end
+
+        for ii=1:nptsy
+
+            if yax(1,ii)<0
+                yax(1,ii) = -1*yax(1,ii);
+            end
+
+            Ex_y(jj,ii) = rf_ex(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
+            Ey_y(jj,ii) = rf_ey(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
+            Ez_y(jj,ii) = rf_ez(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
+
+        end
+    end
+
+end
