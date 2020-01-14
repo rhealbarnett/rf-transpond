3a54b69bd6daefbdedcec0ab6292038bb3e93feb
rhealbarnett <rhealbarnett@gmail.com>
2020-01-15 10:32:22 +1100

    Fixed algorithm for projecting E into x and y.

--- wave_projection.m
+++ wave_projection.m
@@ -22,9 +22,9 @@ function [Ex_x, Ey_x, Ez_x, Ex_y, Ey_y, Ez_y] = wave_projection(xax,yax,kx,ky,rf
                 xax(1,ii) = -1*xax(1,ii);
             end
 
-            Ex_x(jj,ii) = rf_ex(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
-            Ey_x(jj,ii) = rf_ey(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
-            Ez_x(jj,ii) = rf_ez(1,jj)*(cos(kx(1,ii)*xax(1,ii)) + 1i*sin(kx(1,ii)*xax(1,ii)));
+            Ex_x(jj,ii) = real(rf_ex(1,jj)*(exp(1i*kx(1,ii)*xax(1,ii))));
+            Ey_x(jj,ii) = real(rf_ey(1,jj)*(exp(1i*kx(1,ii)*xax(1,ii))));
+            Ez_x(jj,ii) = real(rf_ez(1,jj)*(exp(1i*kx(1,ii)*xax(1,ii))));
 
         end
 
@@ -33,10 +33,10 @@ function [Ex_x, Ey_x, Ez_x, Ex_y, Ey_y, Ez_y] = wave_projection(xax,yax,kx,ky,rf
             if yax(1,ii)<0
                 yax(1,ii) = -1*yax(1,ii);
             end
-
-            Ex_y(jj,ii) = rf_ex(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
-            Ey_y(jj,ii) = rf_ey(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
-            Ez_y(jj,ii) = rf_ez(1,jj)*(cos(ky(1,ii)*yax(1,ii)) + 1i*sin(ky(1,ii)*yax(1,ii)));
+            
+            Ex_y(jj,ii) = real(rf_ex(1,jj)*(exp(1i*ky(1,ii)*yax(1,ii))));
+            Ey_y(jj,ii) = real(rf_ey(1,jj)*(exp(1i*ky(1,ii)*yax(1,ii))));
+            Ez_y(jj,ii) = real(rf_ez(1,jj)*(exp(1i*ky(1,ii)*yax(1,ii))));
 
         end
     end
