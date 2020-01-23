6533861cf842db373417ec9b99a3f4821814cbae
rhealbarnett <rhealbarnett@gmail.com>
2020-01-23 15:51:42 +1100

    Changed to a function.

--- fft_kz.m
+++ fft_kz.m
@@ -1,14 +1,58 @@
 
+function [kz_spec, k_ax, phase] = fft_kz(dx,npts,rf_ex,rf_ey,rf_ez,plots)
 
-dk = 1.0/((npts)*dx);
-knyq = 1.0/(2.0*dx);
-k_np = (npts/2);
-k_ax = zeros(1,k_np);
+    dk = 1.0/((npts-1)*dx);
+    knyq = 1.0/(2.0*dx);
+    k_np = (npts/2);
+    k_ax = zeros(1,k_np);
 
-for ii=1:k_np
-    k_ax(1,ii) = dk*(ii-1);
+    for ii=1:k_np
+
+        k_ax(1,ii) = dk*(ii-1)*2.0*pi;
+
+    end
+
+    fft_Ex = fft(rf_ex)/npts;
+    fft_Ey = fft(rf_ey)/npts;
+    fft_Ez = fft(rf_ez)/npts;
+    
+    kz_spec = [abs(fft_Ex); abs(fft_Ey); abs(fft_Ez)]';
+
+    [mag_ex, idx_ex] = max(abs(fft_Ex(11:npts/2)));
+    [mag_ey, idx_ey] = max(abs(fft_Ey(11:npts/2)));
+    [mag_ez, idx_ez] = max(abs(fft_Ez(11:npts/2)));
+
+    pex = angle(fft_Ex(idx_ex+10));
+    pey = angle(fft_Ey(idx_ey+10));
+    pez = angle(fft_Ez(idx_ez+10));
+
+    phase_xy = (pey - pex)*(180/pi);
+    phase_xz = (pez - pex)*(180/pi);
+    phase_yz = (pey - pez)*(180/pi);
+
+    phase = [phase_xy; phase_xz; phase_yz];
+
+    %%
+
+    if plots
+        figure(1)
+        subplot(3,1,1)
+        plot(k_ax,abs(fft_Ex(1:npts/2)),'k','Linewidth',2)
+        xlim([0 100])
+        set(gca,'xtick',[])
+        ylabel('|FFT[E_x (Vm^{1})]|')
+
+        subplot(3,1,2)
+        plot(k_ax,abs(fft_Ey(1:npts/2)),'k','Linewidth',2)
+        xlim([0 100])
+        set(gca,'xtick',[])
+        ylabel('|FFT[E_y (Vm^{1})]|')
+
+        subplot(3,1,3)
+        plot(k_ax,abs(fft_Ez(1:npts/2)),'k','Linewidth',2)
+        xlim([0 100])
+        xlabel('k_z (m^{1})')
+        ylabel('|FFT[E_z (Vm^{1})]|')
+    end
+
 end
-
-fft_sig = (fft(imag(rf_ey)));
-
-plot(k_ax,abs(fft_sig(1:npts/2))/npts)
