e1a7e62f273ae6cbf076d0809de3027e8ca8678d
rhealbarnett <rhealbarnett@gmail.com>
2020-03-10 12:41:09 -0400

    Included loop to call y power scan data.

--- get_plot_1dIsat.m
+++ get_plot_1dIsat.m
@@ -1,11 +1,11 @@
 %% Call in xz plane Isat data file
 
-filename = '/Volumes/DATA/LAPD/RF_April22/11_Mach_p30xyz_12kV.hdf5';
+filename = '/Volumes/DATA/LAPD/RF_April22/07_Mach_p30y_12kV.hdf5';
 
 
 %% get Mach probe data: check the LAPD_201904_documentation ---------------- %%
 
-probename = 'Mach';
+
 channels = ["Mach1", "Mach2", "Mach3", "Mach4","Mach5","Mach6"];
 
 
@@ -16,26 +16,7 @@ channels = ["Mach1", "Mach2", "Mach3", "Mach4","Mach5","Mach6"];
 % To see an approximation for the flow, take the difference of the paired
 % probes. 
 
-% xz plane near antenna
-% (-11 <= x <= -6) cm, (-12 <= z <= 12) cm
-gcx = -8.5;
-gcz = 0.0;
-
-dx = 0.5;
-dz = 0.25;
-
-Nx = 11;
-Nz = 97;
-
-minx = gcx - ((Nx-1)/2)*dx;
-maxx = gcx + ((Nx-1)/2)*dx;
-
-minz = gcz - ((Nz-1)/2)*dz;
-maxz = gcz + ((Nz-1)/2)*dz;
-
-% x = linspace(minx,maxx,Nx);
-x = -11;
-z = linspace(minz,maxz,Nz);
+z_line = 0;
 
 % attenuation (V) and resistance (omhs), needed to calibrate the signal.
 atten = 3.16;
@@ -51,25 +32,153 @@ tmax = 0.5;
 
 plots = 1;
 
+%%
 
- %% get data ---------------------------------------------------------------- %
+if z_line
+    % xz plane near antenna
+    % (-11 <= x <= -6) cm, (-12 <= z <= 12) cm
+    
+    probename = 'Mach';
+    
+    gcx = -8.5;
+    gcz = 0.0;
 
-[actx_LP, acty_LP, time_LP, data_LP] = get_probe(filename, probename, channels, x, z, dz, 1);
+    dx = 0.5;
+    dz = 0.25;
 
-callib = atten/res;
+    Nx = 11;
+    Nz = 97;
 
-for ii= 1:numel(x)
-    for jj= 1:numel(z)
-        data_LP{ii,jj,1} = atten*data_LP{ii,jj,1} / res; % multiply by attenuated value, divide by resistance
-        data_LP{ii,jj,2} = atten*data_LP{ii,jj,2} / res; 
-        data_LP{ii,jj,3} = atten*data_LP{ii,jj,3} / res; 
-        data_LP{ii,jj,4} = atten*data_LP{ii,jj,4} / res;
-        data_LP{ii,jj,5} = atten*data_LP{ii,jj,5} / res;
-        data_LP{ii,jj,6} = atten*data_LP{ii,jj,6} / res;
+    minx = gcx - ((Nx-1)/2)*dx;
+    maxx = gcx + ((Nx-1)/2)*dx;
+
+    minz = gcz - ((Nz-1)/2)*dz;
+    maxz = gcz + ((Nz-1)/2)*dz;
+
+    % x = linspace(minx,maxx,Nx);
+    x = -11;
+    z = linspace(minz,maxz,Nz);
+
+
+     % get data ---------------------------------------------------------------- %
+
+    [actx_LP, acty_LP, time_LP, data_LP] = get_probe(filename, probename, channels, x, z, dz, 1);
+
+    callib = atten/res;
+
+    for ii= 1:numel(x)
+        for jj= 1:numel(z)
+            data_LP{ii,jj,1} = atten*data_LP{ii,jj,1} / res; % multiply by attenuated value, divide by resistance
+            data_LP{ii,jj,2} = atten*data_LP{ii,jj,2} / res; 
+            data_LP{ii,jj,3} = atten*data_LP{ii,jj,3} / res; 
+            data_LP{ii,jj,4} = atten*data_LP{ii,jj,4} / res;
+            data_LP{ii,jj,5} = atten*data_LP{ii,jj,5} / res;
+            data_LP{ii,jj,6} = atten*data_LP{ii,jj,6} / res;
+        end
     end
+
+elseif ~z_line
+    
+    probename = 'XY[3]: 3-axisMach_Suying#1';
+    
+    gcx = -10.0;
+    gcy = 0.0;
+
+    dx = 0.0;
+    dy = 1.0;
+
+    Nx = 1;
+    Ny = 21;
+
+    minx = gcx - ((Nx-1)/2)*dx;
+    maxx = gcx + ((Nx-1)/2)*dx;
+
+    miny = gcy - ((Ny-1)/2)*dy;
+    maxy = gcy + ((Ny-1)/2)*dy;
+
+    % x = linspace(minx,maxx,Nx);
+    x = -10;
+    y = linspace(miny,maxy,Ny);
+    
+    temp_x = zeros(120832,Ny,12);
+    temp_y = zeros(120832,Ny,12);
+    temp_z = zeros(120832,Ny,12);
+    
+    fft_sigx = zeros(120832,Ny,12);
+    fft_sigy = zeros(120832,Ny,12);
+    fft_sigz = zeros(120832,Ny,12);
+
+    for kk=1:12
+
+        filename = ['/Volumes/DATA/LAPD/RF_April22/07_Mach_p30y_', num2str(kk), 'kV.hdf5'];
+
+        [actx_LP, acty_LP, time_LP, data_LP] = get_probe(filename, probename, channels, x, y, dy/2, 1);
+
+%     [actx_LP, acty_LP, time_LP, data_LP] = get_probe(filename, probename, channels, x, y, dy, 1);
+    
+        for ii= 1:numel(x)
+            for jj= 1:numel(y)
+                data_LP{ii,jj,1} = atten*data_LP{ii,jj,1} / res; % multiply by attenuated value, divide by resistance
+                data_LP{ii,jj,2} = atten*data_LP{ii,jj,2} / res; 
+                data_LP{ii,jj,3} = atten*data_LP{ii,jj,3} / res; 
+                data_LP{ii,jj,4} = atten*data_LP{ii,jj,4} / res;
+                data_LP{ii,jj,5} = atten*data_LP{ii,jj,5} / res;
+                data_LP{ii,jj,6} = atten*data_LP{ii,jj,6} / res;
+            end
+        end
+
+        for ii= 1:numel(x)
+            for jj= 1:numel(y)
+                data_x{ii,jj,1} = (data_LP{ii,jj,1} + data_LP{ii,jj,2})./2.0;
+                data_y{ii,jj,1} = (data_LP{ii,jj,3} + data_LP{ii,jj,4})./2.0;
+                data_z{ii,jj,1} = (data_LP{ii,jj,5} + data_LP{ii,jj,6})./2.0;
+
+                temp_x(:,jj,kk) = data_x{ii,jj,1};
+                temp_y(:,jj,kk) = data_y{ii,jj,1};
+                temp_z(:,jj,kk) = data_z{ii,jj,1};
+
+            end
+        end
+        
+    end
+    
+    npts = length(time_LP);
+    dt = 4.0000e-08;
+    df = 1.0/((npts)*dt);
+    Fnyq = 1.0/(2.0*dt);
+    freq_np = (npts/2);
+    freq_ax = zeros(1,freq_np);
+
+    for ii=1:freq_np
+        freq_ax(1,ii) = df*(ii-1);
+    end
+    
+    bins = find(freq_ax > 1e5);
+    
+    for ii = 1:numel(y)
+        for jj = 1:12
+    
+            fft_sigx(:,ii,jj) = fft(temp_x(:,ii,jj));
+            fft_sigy(:,ii,jj) = fft(temp_y(:,ii,jj));
+            fft_sigz(:,ii,jj) = fft(temp_z(:,ii,jj));
+            
+            fft_sigx(int32(bins),ii,jj) = 0.0;
+            fft_sigx(npts - int32((bins)),ii,jj) = 0.0;
+            inv_sigx(:,ii,jj) = ifft(fft_sigx(:,ii,jj));
+            filt_x(:,ii,jj) = real(inv_sigx(:,ii,jj));
+            fft_sigy(int32(bins),ii,jj) = 0.0;
+            fft_sigy(npts - int32((bins)),ii,jj) = 0.0;
+            inv_sigy(:,ii,jj) = ifft(fft_sigy(:,ii,jj));
+            filt_y(:,ii,jj) = real(inv_sigy(:,ii,jj));
+            fft_sigz(int32(bins),ii,jj) = 0.0;
+            fft_sigz(npts - int32((bins)),ii,jj) = 0.0;
+            inv_sigz(:,ii,jj) = ifft(fft_sigz(:,ii,jj));
+            filt_z(:,ii,jj) = real(inv_sigz(:,ii,jj));
+            
+        end
+    end
+    
 end
-
-
 
 
 %% Calculate average of each probe for density proxy from Isat
@@ -221,7 +330,7 @@ hold on
 
 %% Find and zero FFT values in frequency bins above some value. 
 
-% bins = find(freq_ax > 1e5);
+bins = find(freq_ax > 1e5);
 % fft_sig(int32(bins)) = 0.0;
 % fft_sig(npts - int32((bins))) = 0.0;
 % inv_sig = ifft(fft_sig);
@@ -300,6 +409,23 @@ xlim([min(time_LP) max(time_LP(shift_time))])
 hold off
 
 % export_fig('/Volumes/DATA/thesis/RFT/figs/isat_fft_filtered.png',...
+%     '-r300')
+
+x0 = 0;
+y0 = 0;
+width = 500;
+height = 600;
+figure(3)
+set(gcf,'Position',[x0 y0 width height],'Color','w')
+plot(time_LP(shift_time),temp(shift_time),'k','Linewidth',1.5)
+hold on
+plot(0.5007*ones(1,length(temp(shift_time))),...
+    linspace(0.0,0.025,length(temp(shift_time))),'--r','Linewidth',1.5)
+xlabel('Time (ms)')
+ylabel('I_{\it sat} (A)')
+xlim([min(time_LP) max(time_LP(shift_time))])
+
+% export_fig('/Users/rhealbarnett/Documents/Documents/presentations/2020-rfscidac/isat_filtered.png',...
 %     '-r300')
 
 %% Find indices for each number of RF periods
@@ -423,12 +549,15 @@ for ii=1:Nz
     
 end
 
+ratio = (ratio_x + ratio_y + ratio_z)/3.;
+
 figure(5)
 set(gcf,'Position',[x0 y0 width height],'color','w')
-plot(z,ratio_x,'-.k')
+set(gca,'Fontsize',30)
+plot(z,ratio_x,'-.k','Linewidth',1.5)
 hold on
-plot(z,ratio_y,'-ok')
-plot(z,ratio_z,'-*k')
+plot(z,ratio_y,'-ok','Linewidth',1.5)
+plot(z,ratio_z,'-*k','Linewidth',1.5)
 legend('R_{I{\itsat,x}}','R_{I{\itsat,y}}','R_{I{\itsat,z}}','location','north west')
 xlim([min(z) max(z)])
 xlabel('Axial Position (cm)')
@@ -436,7 +565,24 @@ ylabel('(I_{\itsat} - I_{{\itsat},RF})/I_{\itsat}')
 
 % export_fig('/Volumes/DATA/thesis/RFT/figs/isat_ratio_line.png',...
 %     '-r300')
-    
+
+figure(6)
+set(gcf,'Position',[x0 y0 width height],'color','w')
+set(gca,'Fontsize',30)
+plot(z,ratio,'-*k','Linewidth',1.5)
+hold on
+% legend('R_{I{\itsat,x}}','R_{I{\itsat,y}}','R_{I{\itsat,z}}','location','north west')
+xlim([min(z) max(z)])
+xlabel('Axial Position (cm)')
+ylabel('(I_{\itsat} - I_{{\itsat},RF})/I_{\itsat}')
+plot(-4*ones(1,Nz),linspace(0.2,0.4,Nz),...
+            '--b','Linewidth',1.5,'HandleVisibility','off')
+plot(2*ones(1,Nz),linspace(0.2,0.4,Nz),...
+            '--b','Linewidth',1.5,'HandleVisibility','off')
+ylim([0.2,0.4])
+
+% export_fig('/Users/rhealbarnett/Documents/Documents/presentations/2020-rfscidac/isat_ratio_avg_line.png',...
+%     '-r300')
 
 %% Isat image plots
     
