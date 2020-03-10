07ba2d4e014b1481c3d4c56bc509e52a0141e052
rhealbarnett <rhealbarnett@gmail.com>
2020-03-10 12:45:40 -0400

    Included call to ponderomotive function for plotting.

--- wave_test.m
+++ wave_test.m
@@ -7,10 +7,10 @@ lapd_params;
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,n_new,m_s,om,eps0,npts,1);
 [A,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(zax,ky,kx,k0,...
 om,mu0,cpdt,sig,source,0,1,1);
-Ex = interp1(zax,rf_ex,vxax,'linear');
-Ey = interp1(zax,rf_ey,vxax,'linear');
-Ez = interp1(zax,rf_ez,vxax,'linear');
-pf_source = pond_source({'total',0},{Ex,Ey,Ez},m_s,q_s,om_c,om,vdx,1,{0,vxax});
+% Ex = interp1(zax,rf_ex,vxax,'linear');
+% Ey = interp1(zax,rf_ey,vxax,'linear');
+% Ez = interp1(zax,rf_ez,vxax,'linear');
+% [Ediff, pf_source] = pond_source({'para',0},{Ex,Ey,Ez},m_s,q_s,om_c,om,vdx,1,{1,vxax});
 wave_sol_plots
 
 %%
