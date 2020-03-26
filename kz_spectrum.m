2913fd09dde637c73a21af991fdd6683a7d1e774
rhealbarnett <rhealbarnett@gmail.com>
2020-03-25 22:06:01 -0400

    Small edits to create thesis figures.

--- kz_spectrum.m
+++ kz_spectrum.m
@@ -21,8 +21,8 @@ for ii=1:npts
     
     [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,density,m_s,om,eps0,npts,1);
     [A,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(zax,ky,kx,k0,...
-    om,mu0,cpdt,source,0,1,1);
-
+    om,mu0,cpdt,sig,source,0,1,1);
+                                      
     [kz_spec, k_ax, phase] = fft_kz(dx,npts,rf_ex,rf_ey,rf_ez,plots);
 %     [kz_spec, k_ax, phase] = fft_kz(dx,NP,rf_ex(indx),rf_ey(indx),rf_ez(indx),plots);
     
@@ -36,8 +36,8 @@ end
 
 x0 = 0;
 y0 = 0;
-width = 950;
-height = 650;
+width = 1000;
+height = 500;
 
 indk = find(k_ax<=50);
 
@@ -59,18 +59,18 @@ set(gcf,'Position',[x0 y0 width height],'Color','w')
 levels = linspace(0,1.e-4,50);
 contourf(log10(n_new),k_ax(indk),(kz_spec_density(:,indk))',levels,'Linecolor','none')
 hold on
-plot(log10(n_new), real(kp11),'--g','Linewidth',0.8)
+plot(log10(n_new), real(kp11),'-.r','Linewidth',2)
 c = colorbar;
-colormap(flipud(bone))
-caxis([0.0 1.e-4])
+colormap(magma)
+caxis([0 1.0e-4])
 ylim([0 50])
 yticks(linspace(0,50,11))
 set(gca,'colorscale','linear')
 ylabel('{\it k_z} (m^{-1})')
 xlabel('log_{10}({\itn} (m^{-3}))')
-ylabel(c,'|FFT[{\it E_z} (Vm^{-1})]|','Fontsize',20)
+ylabel(c,'|FFT[{\it E_z} (Vm^{-1})]|','Fontsize',30)
 
-% export_fig('/Volumes/DATA/thesis/RFT/figs/kvsn_dispersioncont_linearscalebone.png',...
+% export_fig('/Volumes/DATA/thesis/RFT/figs/kvsn_dispersioncont_linearscale.png',...
 %     '-r300')
 
 %%
