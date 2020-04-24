dd2e83bc2cf486f550fa20f2f1cb899e62047650
rhealbarnett <rhealbarnett@gmail.com>
2020-04-24 14:38:58 -0400

    Removed diss_pow input from call to wave solver function.

--- lapd_equib.m
+++ lapd_equib.m
@@ -172,7 +172,7 @@ vx_init = vx_new;
 n_new_uni = interp1(nxax,n_new,zax,'linear');
 
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,n_new_uni,m_s,om,eps0,npts,1);
-[A,rf_e,rf_ex,rf_ey,rf_ez,diss_pow] = wave_sol(zax,ky,kx,k0,...
+[A,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(zax,ky,kx,k0,...
     om,mu0,cpdt,sig,source,0,1,0);
 
 Efield = abs(rf_ez).^2;
