47f3d9fc7b04f7b1b1be5dc2a7bca00507d6a18a
rhealbarnett <rhealbarnett@gmail.com>
2020-06-03 17:24:38 +1000

    Added second loop, over density values.

--- scan.sh
+++ scan.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 MATLAB=/Applications/MATLAB_R2018b.app/bin/matlab
 
-for hh in {10..20..5}
+for hh in {10..25..5}
+do
+for ww in {0.5e17,0.7925e17,1.256e17,1.9907e17,3.155e17,5.0e17}
 do 
-$MATLAB -noFigureWindows -batch "coupled_rf_transp($hh)" &
-#echo "$hh"
+$MATLAB -noFigureWindows -batch "coupled_rf_transp($hh,$ww)" &
+#echo "$hh $ww"
+done
 done
 
