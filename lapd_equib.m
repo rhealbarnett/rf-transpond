90efd7fe42db814b1dead21bf4d9366d99e11697
rhealbarnett <rhealbarnett@gmail.com>
2020-06-24 12:42:46 +1000

    Edited source input to Gaussian 2nd deriv in call to wave sol function.

--- lapd_equib.m
+++ lapd_equib.m
@@ -174,7 +174,7 @@ n_new_uni = interp1(nxax,n_new,zax,'linear');
 
 [om_c,om_p,cpdt,s_arr,d_arr,p_arr,sig] = dielec_tens(q_s,B0,n_new_uni,m_s,om,eps0,npts,1);
 [A,rf_e,rf_ex,rf_ey,rf_ez] = wave_sol(zax,ky,kx,k0,...
-    om,mu0,cpdt,source,0,1,1,0);
+    om,mu0,cpdt,source_sec,0,1,1,0);
 poyn = poynting(rf_ex, rf_ey, rf_ez, kx, ky, zax, om);
 
 Efield = abs(rf_ez).^2;
