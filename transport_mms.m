8bd25726e35a1458e01ddd4a96af5374729072dc
rhealbarnett <rhealbarnett@gmail.com>
2020-10-28 13:32:22 +1100

    Commented out npts to run SS MMS test.

--- transport_mms.m
+++ transport_mms.m
@@ -32,7 +32,7 @@ variable = 1;
 % include two additional gridpoints for the density ghost points
 % velocity grid will then be defined as having npts-1 (xax(1:npts-1)) --
 % density solution space will be defined as having npts-2 (xax(2:npts-1))
-npts = 512;
+% npts = 512;
 dx = (xmax - xmin)/(npts - 1);
 nxax = linspace(xmin-0.5*dx,xmax+0.5*dx,npts);
 vxax = linspace(xmin,xmax,npts-1);
