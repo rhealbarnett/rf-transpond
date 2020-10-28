7ac29a704f823fc301247ba6c29311437586143f
rhealbarnett <rhealbarnett@gmail.com>
2020-10-28 16:12:02 +1100

    Adding/editing comments, up to line 84.

--- transport_mms.m
+++ transport_mms.m
@@ -25,11 +25,9 @@ xmax = 0.4;
 % Turn variable grid on (1) or off (0)
 variable = 1;
 
-% include two additional gridpoints for the density ghost points
-% velocity grid will then be defined as having npts-1 (xax(1:npts-1)) --
-% density solution space will be defined as having npts-2 (xax(2:npts-1))
-% npts = 512;
-dx = (xmax - xmin)/(npts - 1);
+% Define uniform grids. For a staggered grid, n has npts and v has npts-1.
+% The number of points (npts) should be set in the run_mms.m function.
+dx = (xmax - xmin)/(npts);
 nxax = linspace(xmin-0.5*dx,xmax+0.5*dx,npts);
 vxax = linspace(xmin,xmax,npts-1);
 ndx = dx*ones(1,npts-1);
@@ -38,40 +36,45 @@ vdx = dx*ones(1,npts-2);
 
 %%
 %----------------------------------------------------------------------%
-% non uniform grid calculation
+% Non-uniform grid calculation
 %----------------------------------------------------------------------%
 
 if variable
     
     clear vxax nxax
-
-    % location of sign change
+    
+    %--
+    % Location of sign change
     xc = (xmax - xmin)/2.0;
     
-    %'strength' of grid refinement.
-    % sign also indicates whether refinement is in the centre or at the
-    % boundaries
+    %--
+    % 'Strength' of grid refinement.
+    % Sign also indicates whether refinement is in the centre or at the
+    % boundaries.
     A = -1.0;
 
-    % set up the unit spaced parameter, xi, that the grid is a function of
+    %--
+    % Set up the unit spaced parameter, xi, that the grid is a function of.
     smax = 1.0;
     smin = 0.0;
     s = linspace(smin,smax,npts-1); 
 
-    % calculate the x values from xi
+    %--
+    % Calculate the x values from xi.
     x = xc*(1.0 - tanh(A*(1.0 - 2.0*s))./tanh(A));
 
+    %--
+    % Set vxax based on the variable x grid, shifted by xmin.
     vxax = x - abs(xmin);
     vdx = (vxax(2:end) - vxax(1:end-1));
-
-%     npts = length(vxax) + 1;
+    
+    %--
+    % Reinitialise nxax and calculate based on vxax. 
     nxax = zeros(1,npts);
-
     nxax(1) = vxax(1) - 0.5*vdx(1);
     nxax(end) = vxax(end) + 0.5*vdx(end);
     nxax(2:end-1) = (vxax(2:end) + vxax(1:end-1))/2.0;
     ndx = nxax(2:end) - nxax(1:end-1);
-    
     
 end
 
