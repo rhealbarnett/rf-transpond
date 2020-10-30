3f4b2f8d178db2e11f24d4e5645b35b86d5abdf7
rhealbarnett <rhealbarnett@gmail.com>
2020-10-30 14:47:28 +1100

    Deleted old commented out code.

--- constants.m
+++ constants.m
@@ -2,14 +2,8 @@
 % physical constants                                    %
 %-------------------------------------------------------%
 
-% e = 1.6022e-19;
-% mp = 1.67e-27;
-% me = 9.11e-31;
-% kb = 1.38e-23;
-% mu0 = 4.0*pi*1.0e-7;
-% eps0 = 8.85e-12;
-% c0 = 1.0/sqrt(eps0*mu0);
 function const = constants()
+
     const.e = 1.6022e-19;
     const.mp = 1.67e-27;
     const.me = 9.11e-31;
