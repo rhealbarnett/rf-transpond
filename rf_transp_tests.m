19255c2d6de925467bbaba90b35e9d06fb37ad34
rhealbarnett <rhealbarnett@gmail.com>
2020-11-04 13:52:38 +1100

    Added transport mms test.

--- rf_transp_tests.m
+++ rf_transp_tests.m
@@ -4,3 +4,9 @@ function tests = rf_transp_tests
 
 end
 
+function [] = mms_test(testCase)
+
+    [linf_arr,ltwo_arr,ratio_inf_arr,ratio_two_arr,...
+    oo_inf_arr,oo_two_arr] = run_mms(0,1,1,0);
+
+end
