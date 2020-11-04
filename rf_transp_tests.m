1f8e56a43ff32382c679221439c78fa66f522349
rhealbarnett <rhealbarnett@gmail.com>
2020-11-04 13:58:14 +1100

    Included comparison between test case and calculated.

--- rf_transp_tests.m
+++ rf_transp_tests.m
@@ -7,6 +7,13 @@ end
 function [] = mms_test(testCase)
 
     [linf_arr,ltwo_arr,ratio_inf_arr,ratio_two_arr,...
-    oo_inf_arr,oo_two_arr] = run_mms(0,1,1,0);
+    oo_inf_arr,oo_two_arr,arr] = run_mms(0,1,1,0);
+
+    p = polyfit(log10(arr),log10(oo_two_arr),1);
+
+    expSlope = 1.0;
+    actSlope = p(1);
+    
+    verifyEqual(testCase,actSlope,expSlope,'RelTol',1e-1)
 
 end
