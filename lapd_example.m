cb05767faa7dadb0ba1eac345f98458585e1f735
rhealbarnett <rhealbarnett@gmail.com>
2020-11-27 16:52:56 +1100

    Example case for the coupled rf wave and transport code.

---
+++ lapd_example.m
@@ -0,0 +1,12 @@
+% ------------------------------------------------------------------- %
+% Run example of coupling RF wave and transport solvers.
+% Use LAPD as example device. 
+% 201126 rlbarnett
+% ------------------------------------------------------------------- %
+
+lapd_equil
+wave_sol_plots
+plots = 0;
+transport_1d
+wave_sol_plots
+
