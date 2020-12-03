64c0e286991eddc5c474eeac7642a6c692a0c522
rhealbarnett <rhealbarnett@gmail.com>
2020-12-03 15:55:23 +1100

    Edit to figure save path

--- kz_spectrum.m
+++ kz_spectrum.m
@@ -57,7 +57,7 @@ function [actkz,dk] = kz_spectrum(n_new,q_s,m_s,om,npts,damp_len,dampFac,zax,ky,
         xlabel('log_{10}({\itn} (m^{-3}))')
         ylabel(c,'|FFT[{\it E_z} (Vm^{-1})]|','Fontsize',30)
         
-        print('-f5',strcat(filepath,'outputs/wave_verification',num2str(ii)),'-dpng');
+        print('-f5',strcat('outputs/wave_verification',num2str(ii)),'-dpng');
         close 5
     end
 
